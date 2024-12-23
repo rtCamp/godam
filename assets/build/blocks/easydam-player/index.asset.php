454cf129071feb8e762b7190b4dc5c9edc87964a
prachigarg19 <prachi.garg@rtcamp.com>
2024-12-23 20:39:25 +0530

    delete build folder

--- assets/build/blocks/easydam-player/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => '811f76f1f664491d2edf');
