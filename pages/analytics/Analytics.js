cc734895a43cb6f31719db5f0319b6ca5936292c
Prachi Garg <72646230+prachigarg19@users.noreply.github.com>
2025-01-08 14:41:29 +0530

    Analytics page refactor (#79)
    * Changes made:
    1. Add padding top
    2. Add border around heatmap only if data is not present
    3. Add videojs component
    
    * style: fix width of large videos
    
    * style: fix aspect ratio of video
    
    * Changes made:
    1. style: confine width and height of large dimension videos
    2. feat: handle empty meta data edge case
    
    * fix: fix aspect ratio issue through options setup
    
    * feat: add video title

--- pages/analytics/Analytics.js
+++ pages/analytics/Analytics.js
@@ -7,15 +7,25 @@ import React, { useState, useEffect } from 'react';
  */
 import './analytics.css';
 import '../video-editor/style.scss';
+import VideoJS from './Video';
+import axios from 'axios';
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
 
 const Chart = ( { data, duration, width } ) => {
-	// Calculate the adjuster and opacity once
-	const adjuster = width / duration;
+	const updatedWidth = 640 < width ? 640 : width;
+	const adjuster = updatedWidth / duration;
 	const opacity = ( 100 / data.length ) / 100;
 
 	return (
-		<div id="chart" className="chart mt-6 h-24" style={ { width } }>
-			{ data && data.map( ( ranges, rangeIndex ) =>
+		<div id="chart" className="chart mt-6 h-24" style={ {
+			width: updatedWidth,
+			border: data && data.ranges ? 'none' : '1px solid #b7b4b4', // Add border if no data
+		} }>
+			{ data && data.ranges &&
+			data.ranges.map( ( ranges, rangeIndex ) =>
 				ranges.map( ( range, index ) => (
 					<div
 						key={ `${ rangeIndex }-${ index }` }
@@ -34,50 +44,67 @@ const Chart = ( { data, duration, width } ) => {
 
 const Analytics = ( { attachmentID } ) => {
 	const [ analyticsData, setAnalyticsData ] = useState( null );
-	const [ currentTime, setCurrentTime ] = useState( 0 );
 
 	useEffect( () => {
-		const fetchSettings = async () => {
-			const url = `/wp-json/wp/v2/media/${ attachmentID }`;
-			try {
-				const analyticsResponse = await fetch( url, {
-					headers: {
-						'Content-Type': 'application/json',
-						'X-WP-Nonce': window.wpApiSettings.nonce,
-					},
-				} );
+		const url = `/wp-json/wp/v2/media/${ attachmentID }`;
 
-				const data = await analyticsResponse.json();
-
+		axios
+			.get( url )
+			.then( ( response ) => {
+				const data = response.data;
 				setAnalyticsData( data );
-			} catch ( error ) {
+			} )
+			.catch( ( error ) => {
 				console.error( 'Failed to fetch data:', error );
-			}
-		};
-
-		fetchSettings();
+			} );
 	}, [ attachmentID ] );
+
+	const metadata = analyticsData?.easydam_meta?.videoConfig
+		? analyticsData.easydam_meta.videoConfig
+		: {
+			controls: true,
+			autoplay: false,
+			preload: 'auto',
+			fluid: true,
+			aspectRatio: '16:9',
+		};
 
 	return (
 		<div className="analytics-container flex flex-col items-center">
 			<div className="video-info-container">
 				<div className="analytics-info-container">
+					{ analyticsData?.title?.rendered && (
+						<h1 className="mb-4">{ analyticsData?.title?.rendered }</h1>
+					) }
 					<div className="analytics-info">
 						<span>62%</span>
-						<p>Average Engagement</p>
+						<p>{ __( 'Average Engagement', 'transcoder' ) }</p>
 					</div>
 					<hr />
 					<div className="analytics-info">
 						<span>104</span>
-						<p>Total Plays</p>
+						<p>{ __( 'Total Plays', 'transcoder' ) }</p>
 					</div>
 					<hr />
 					<div className="analytics-info">
 						<span>28%</span>
-						<p>Play Rate</p>
+						<p>{ __( 'Play Rate', 'transcoder' ) }</p>
 					</div>
 					<hr />
 				</div>
+				{ analyticsData && (
+					<VideoJS
+						options={ {
+							...metadata,
+							sources: [
+								{
+									src: analyticsData.source_url || '',
+									type: analyticsData.mime_type || 'video/mp4',
+								},
+							],
+						} }
+					/>
+				) }
 			</div>
 			{ analyticsData && (
 				<Chart
