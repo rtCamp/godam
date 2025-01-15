2f52bc748c09342cf6fcac9abb538f41d781fb30
Juzar Bharmal <53657281+Juzar10@users.noreply.github.com>
2025-01-15 13:30:05 +0530

    feat: disable the progress bar for the grid view. (#88)

--- assets/src/js/media-library/transcoding-status.js
+++ assets/src/js/media-library/transcoding-status.js
@@ -1,130 +1,132 @@
 /**
  * External dependencies
  */
-import DOMPurify from 'isomorphic-dompurify';
+// import DOMPurify from 'isomorphic-dompurify';
+/**
+ * JS for monitoring the transcoding progress of video files in the media library.
+ */
+// ( function() {
+// 	document.addEventListener( 'DOMContentLoaded', function() {
+// 		if ( typeof wp === 'undefined' || typeof wp.Uploader === 'undefined' ) {
+// 			return;
+// 		}
 
-( function() {
-	document.addEventListener( 'DOMContentLoaded', function() {
-		if ( typeof wp === 'undefined' || typeof wp.Uploader === 'undefined' ) {
-			return;
-		}
+// 		const progressBars = new Map();
 
-		const progressBars = new Map();
+// 		// Check for existing video files in the media library.
+// 		setTimeout( checkExistingVideoProgress, 500 );
 
-		// Check for existing video files in the media library.
-		setTimeout( checkExistingVideoProgress, 500 );
+// 		// Listen for new files added to the queue.
+// 		wp.Uploader.queue.on( 'add', function( file ) {
+// 			wp.Uploader.queue.on( 'reset', function() {
+// 				// Only show progress bar for video files.
+// 				if ( file.attributes.type === 'video' ) {
+// 					const attachmentId = file.id;
+// 					initializeProgressBar( attachmentId );
+// 				}
 
-		// Listen for new files added to the queue.
-		wp.Uploader.queue.on( 'add', function( file ) {
-			wp.Uploader.queue.on( 'reset', function() {
-				// Only show progress bar for video files.
-				if ( file.attributes.type === 'video' ) {
-					const attachmentId = file.id;
-					initializeProgressBar( attachmentId );
-				}
+// 				progressBars.clear();
+// 				checkExistingVideoProgress();
+// 			} );
+// 		} );
 
-				progressBars.clear();
-				checkExistingVideoProgress();
-			} );
-		} );
+// 		/**
+// 		 * Initialize progress bar for the given attachment.
+// 		 *
+// 		 * @param {number} attachmentId
+// 		 */
+// 		function initializeProgressBar( attachmentId ) {
+// 			const mediaItemPreview = document.querySelector( `.attachments .attachment[data-id="${ attachmentId }"] .attachment-preview` );
 
-		/**
-		 * Initialize progress bar for the given attachment.
-		 *
-		 * @param {number} attachmentId
-		 */
-		function initializeProgressBar( attachmentId ) {
-			const mediaItemPreview = document.querySelector( `.attachments .attachment[data-id="${ attachmentId }"] .attachment-preview` );
+// 			if ( ! mediaItemPreview ) {
+// 				return;
+// 			}
 
-			if ( ! mediaItemPreview ) {
-				return;
-			}
+// 			const progressBarTemplate =
+// 				`<div class="transcoder-progress-bar" style="display: none;">
+// 					<div class="progress" style="width: 0%;">
+// 						<span class="progress-text">0%</span>
+// 					</div>
+// 				</div>`;
 
-			const progressBarTemplate =
-				`<div class="transcoder-progress-bar" style="display: none;">
-					<div class="progress" style="width: 0%;">
-						<span class="progress-text">0%</span>
-					</div>
-				</div>`;
+// 			const progressBar = DOMPurify.sanitize( progressBarTemplate );
 
-			const progressBar = DOMPurify.sanitize( progressBarTemplate );
+// 			mediaItemPreview.insertAdjacentHTML( 'beforeend', progressBar );
+// 			const progressBarElement = mediaItemPreview.querySelector( '.transcoder-progress-bar' );
+// 			progressBars.set( attachmentId, progressBarElement );
+// 			monitorProgress( attachmentId );
+// 		}
 
-			mediaItemPreview.insertAdjacentHTML( 'beforeend', progressBar );
-			const progressBarElement = mediaItemPreview.querySelector( '.transcoder-progress-bar' );
-			progressBars.set( attachmentId, progressBarElement );
-			monitorProgress( attachmentId );
-		}
+// 		/**
+// 		 * Check for existing video files in the media.
+// 		 */
+// 		function checkExistingVideoProgress() {
+// 			const videoQuery = wp.media.query( {
+// 				type: 'video',
+// 				posts_per_page: -1,
+// 			} );
 
-		/**
-		 * Check for existing video files in the media.
-		 */
-		function checkExistingVideoProgress() {
-			const videoQuery = wp.media.query( {
-				type: 'video',
-				posts_per_page: -1,
-			} );
+// 			videoQuery.more().done( function() {
+// 				const videoAttachments = videoQuery.models;
+// 				videoAttachments.forEach( function( attachment ) {
+// 					const attachmentId = attachment.id;
+// 					initializeProgressBar( attachmentId );
+// 				} );
+// 			} ).fail( function() {
+// 				console.error( 'Failed to load video attachments.' );
+// 			} );
+// 		}
 
-			videoQuery.more().done( function() {
-				const videoAttachments = videoQuery.models;
-				videoAttachments.forEach( function( attachment ) {
-					const attachmentId = attachment.id;
-					initializeProgressBar( attachmentId );
-				} );
-			} ).fail( function() {
-				console.error( 'Failed to load video attachments.' );
-			} );
-		}
+// 		/**
+// 		 * Monitor the transcoding progress of the given attachment.
+// 		 *
+// 		 * @param {number} attachmentId
+// 		 */
+// 		function monitorProgress( attachmentId ) {
+// 			const progressBar = progressBars.get( attachmentId );
+// 			if ( ! progressBar ) {
+// 				return;
+// 			}
 
-		/**
-		 * Monitor the transcoding progress of the given attachment.
-		 *
-		 * @param {number} attachmentId
-		 */
-		function monitorProgress( attachmentId ) {
-			const progressBar = progressBars.get( attachmentId );
-			if ( ! progressBar ) {
-				return;
-			}
+// 			fetch( `${ transcoderSettings.restUrl }/${ attachmentId }`, {
+// 				method: 'GET',
+// 				headers: {
+// 					'X-WP-Nonce': transcoderSettings.nonce,
+// 				},
+// 			} )
+// 				.then( ( response ) => response.json() )
+// 				.then( ( data ) => {
+// 					const progress = parseFloat( data.progress ) || 0;
+// 					progressBar.style.display = 'block';
+// 					const progressBarElement = progressBar.querySelector( '.progress' );
+// 					const progressText = progressBar.querySelector( '.progress-text' );
 
-			fetch( `${ transcoderSettings.restUrl }/${ attachmentId }`, {
-				method: 'GET',
-				headers: {
-					'X-WP-Nonce': transcoderSettings.nonce,
-				},
-			} )
-				.then( ( response ) => response.json() )
-				.then( ( data ) => {
-					const progress = parseFloat( data.progress ) || 0;
-					progressBar.style.display = 'block';
-					const progressBarElement = progressBar.querySelector( '.progress' );
-					const progressText = progressBar.querySelector( '.progress-text' );
+// 					progressBarElement.style.width = `${ progress }%`;
 
-					progressBarElement.style.width = `${ progress }%`;
+// 					// Hide the text if progress is less than 30 to avoid overlapping with the progress bar.
+// 					if ( progress < 30 ) {
+// 						progressText.textContent = '';
+// 					} else {
+// 						progressText.textContent = `${ progress }%`;
+// 					}
 
-					// Hide the text if progress is less than 30 to avoid overlapping with the progress bar.
-					if ( progress < 30 ) {
-						progressText.textContent = '';
-					} else {
-						progressText.textContent = `${ progress }%`;
-					}
-
-					if ( progress < 100 ) {
-						setTimeout( () => monitorProgress( attachmentId ), 1000 );
-					} else {
-						progressBar.remove();
-						progressBars.delete( attachmentId );
-					}
-				} )
-				.catch( ( error ) => {
-					console.error( 'Error monitoring progress:', error );
-					setTimeout( () => monitorProgress( attachmentId ), 1000 );
-				} );
-		}
-	} );
-}() );
+// 					if ( progress < 100 ) {
+// 						setTimeout( () => monitorProgress( attachmentId ), 1000 );
+// 					} else {
+// 						progressBar.remove();
+// 						progressBars.delete( attachmentId );
+// 					}
+// 				} )
+// 				.catch( ( error ) => {
+// 					console.error( 'Error monitoring progress:', error );
+// 					setTimeout( () => monitorProgress( attachmentId ), 1000 );
+// 				} );
+// 		}
+// 	} );
+// }() );
 
 /**
- * Js for sending the REST API request on the button click.
+ * JS for sending the REST API request on the button click.
  */
 ( function() {
 	document.addEventListener( 'DOMContentLoaded', function() {
