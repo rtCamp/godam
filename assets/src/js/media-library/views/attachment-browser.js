149847d9237c15f9870fe22d67ab1dcd618a2966
Juzar Bharmal <53657281+Juzar10@users.noreply.github.com>
2025-02-07 11:31:51 +0530

    comment out the upload to S3 button

--- assets/src/js/media-library/views/attachment-browser.js
+++ assets/src/js/media-library/views/attachment-browser.js
@@ -5,7 +5,7 @@
  */
 import MediaLibraryTaxonomyFilter from './filters/media-library-taxonomy-filter';
 import MediaDateRangeFilter from './filters/media-date-range-filter';
-import MediaUploadToS3 from './filters/media-upload-to-s3';
+// import MediaUploadToS3 from './filters/media-upload-to-s3';
 
 const AttachmentsBrowser = wp?.media?.view?.AttachmentsBrowser;
 
@@ -45,16 +45,18 @@ export default AttachmentsBrowser?.extend( {
 			);
 		}
 
-		if ( MediaUploadToS3 && ! wp?.media?.frame?.el ) {
-			this.toolbar.set(
-				'MediaUploadToS3',
-				new MediaUploadToS3( {
-					controller: this.controller,
-					model: this.collection.props,
-					priority: -75,
-				} ).render(),
-			);
-		}
+		// Comment out the S3 button code until confirmed.
+
+		// if ( MediaUploadToS3 && ! wp?.media?.frame?.el ) {
+		// 	this.toolbar.set(
+		// 		'MediaUploadToS3',
+		// 		new MediaUploadToS3( {
+		// 			controller: this.controller,
+		// 			model: this.collection.props,
+		// 			priority: -75,
+		// 		} ).render(),
+		// 	);
+		// }
 
 		setTimeout( () => {
 			$( '.media-frame' ).removeClass( 'hide-menu' );
