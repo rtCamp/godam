f3f6a2fb09a273eb7886fbb80e59b1fc4c87c806
Juzar Bharmal <53657281+Juzar10@users.noreply.github.com>
2025-02-10 12:35:04 +0530

    Video Editor page changes [develop] (#122)
    * feat: video editor page changes and allow to have video selection
    
    * feat: add pagination for the video editor
    
    * feat: refactor the code for the video editor styles.
    
    * feat: add edit Icon
    
    * fix: color accent issue
    
    * fix: hide transcoded URL from showing on images

---
+++ pages/video-editor/AttachmentGrid.jsx
@@ -0,0 +1,156 @@
+/**
+ * External dependencies
+ */
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { useGetVideosMutation } from './redux/api/video';
+
+const AttachmentGrid = ( { handleAttachmentClick } ) => {
+	const [ getVideos, { isLoading } ] = useGetVideosMutation();
+	const [ attachments, setAttachments ] = useState( [] );
+	const [ search, setSearch ] = useState( '' );
+	const [ page, setPage ] = useState( 1 );
+	const [ hasMore, setHasMore ] = useState( true );
+	const observer = useRef();
+
+	/**
+	 * fetch the videos when the component mounts and when values are changed.
+	 */
+	useEffect( () => {
+		if ( attachments.length > 0 && attachments.length !== 40 ) {
+			return;
+		}
+
+		const videoTimeout = setTimeout( () => {
+			getVideos( { search, page } ).then( ( response ) => {
+				const newVideos = response?.data?.data || [];
+				setAttachments( ( prev ) => ( page === 1 ? newVideos : [ ...prev, ...newVideos ] ) );
+				setHasMore( newVideos.length > 0 );
+			} );
+		}, 500 );
+
+		return () => clearTimeout( videoTimeout );
+	}, [ search, page, getVideos, attachments ] );
+
+	/**
+	 * Intersection observer to load more videos when user scrolls to the bottom
+	 */
+	const lastVideoRef = useCallback(
+		( node ) => {
+			if ( isLoading || ! hasMore ) {
+				return;
+			}
+
+			if ( observer.current ) {
+				observer.current.disconnect();
+			}
+
+			observer.current = new IntersectionObserver( ( entries ) => {
+				if ( entries[ 0 ].isIntersecting ) {
+					setPage( ( prevPage ) => prevPage + 1 );
+				}
+			} );
+
+			if ( node ) {
+				observer.current.observe( node );
+			}
+		},
+		[ isLoading, hasMore ],
+	);
+
+	return (
+		<>
+			<header>
+				<div className="bg-white border-b -ml-[32px] pl-[32px] flex gap-4">
+					<div className="pl-4 pr-9 flex items-center justify-between">
+						<h1 className="py-6 m-0 text-4xl leading-4 font-semibold text-slate-900 flex items-center">
+							{ __( 'Video Editor', 'godam' ) }
+						</h1>
+					</div>
+
+					<div className="flex items-center">
+						<input
+							type="text"
+							placeholder="Search for media"
+							className="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md h-10 min-w-64 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+							onChange={ ( e ) => {
+								setSearch( e.target.value );
+								setPage( 1 );
+								setAttachments( [] );
+							} }
+							value={ search }
+						/>
+					</div>
+				</div>
+			</header>
+
+			<div className="p-4 bg-gray-100 dark:bg-gray-800">
+				{ isLoading && attachments.length === 0 && page === 1 && (
+					<p className="text-center text-gray-500 text-2xl dark:text-gray-400">Loading videos...</p>
+				) }
+
+				{ ! isLoading && attachments.length === 0 && (
+					<p className="text-center text-gray-500 text-2xl dark:text-gray-400">No videos found.</p>
+				) }
+
+				{ attachments.length > 0 && (
+					<div className="flex flex-wrap gap-4">
+						{ attachments.map( ( attachment, index ) => {
+							const isLastItem = index === attachments.length - 1;
+							return (
+								<div
+									key={ attachment.id }
+									ref={ isLastItem ? lastVideoRef : null }
+									role="button"
+									tabIndex="0"
+									aria-label={ `Open ${ attachment.title }` }
+									className="relative p-4 w-40 h-40 md:w-32 md:h-32 flex justify-center align-center bg-white dark:bg-gray-900 rounded-lg shadow-md border border-gray-300 dark:border-gray-700 overflow-hidden group cursor-pointer"
+									onClick={ () => handleAttachmentClick( attachment.id ) }
+									onKeyDown={ ( e ) => {
+										if ( e.key === 'Enter' || e.key === ' ' ) {
+											handleAttachmentClick( attachment.id );
+										}
+									} }
+								>
+
+									{ /* Edit Icon */ }
+									<button
+										className="absolute top-2 right-2 bg-white p-1 rounded-full shadow-md"
+									>
+										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="size-6">
+											<path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+										</svg>
+
+									</button>
+
+									<img
+										src={ attachment.image.src }
+										alt={ attachment.title }
+										className="object-cover transition-transform duration-300 group-hover:scale-105"
+									/>
+									<div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-1 text-center">
+										<p className="text-xs m-0 font-semibold break-words">{ attachment.title }</p>
+									</div>
+								</div>
+							);
+						} ) }
+					</div>
+				) }
+
+				{ isLoading && page > 1 && (
+					<p className="text-center text-gray-500 dark:text-gray-400 mt-4">Loading more videos...</p>
+				) }
+			</div>
+		</>
+	);
+};
+
+export default AttachmentGrid;
---
+++ pages/video-editor/redux/api/video.js
@@ -0,0 +1,45 @@
+/**
+ * External dependencies
+ */
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const videosAPI = createApi( {
+	reducerPath: 'videoAPI',
+	baseQuery: fetchBaseQuery( { baseUrl: '/wp-admin' } ),
+	endpoints: ( builder ) => ( {
+
+		getVideos: builder.mutation( {
+			query: ( data ) => {
+				const params = new URLSearchParams();
+				params.append( 'action', 'query-attachments' );
+				params.append( 'query[post_mime_type]', 'video' );
+				params.append( 'query[posts_per_page]', '40' );
+				params.append( 'query[orderby]', 'date' );
+				params.append( 'query[order]', 'DESC' );
+
+				if ( data.page ) {
+					params.append( 'query[paged]', data.page );
+				} else {
+					params.append( 'query[paged]', '1' );
+				}
+
+				if ( data.search ) {
+					params.append( 'query[s]', data.search );
+				}
+
+				return {
+					url: 'admin-ajax.php',
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+					},
+					body: params.toString(),
+				};
+			},
+		} ),
+	} ),
+} );
+
+export const {
+	useGetVideosMutation,
+} = videosAPI;
