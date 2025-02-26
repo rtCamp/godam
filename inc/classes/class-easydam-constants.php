050aac2e48e861deeb1587d41e800d628453789f
Subodh Rajpopat <58080782+subodhr258@users.noreply.github.com>
2025-02-26 15:31:33 +0530

    Analytics UI and Endpoint Update (#167)
    * feat: Add Banner and tooltips to analytics page
    
    * feat: Update Button color to theme color in GodamHeader and Modal
    
    * feat: Update tooltip icon color
    
    * feat: Update Godam Analytics Endpoint Base Constant

--- inc/classes/class-easydam-constants.php
+++ inc/classes/class-easydam-constants.php
@@ -18,10 +18,4 @@ class EasyDAM_Constants {
 	 * @var string
 	 */
 	const S3_STORAGE_OPTIONS = 'easydam_storage_aws';
-
-	/**
-	 * Analytics Microservice URLs
-	 */
-	const ANALYTICS_ENDPOINT_DEV = 'https://analytics.godam.rt.gw/';
-	// const ANALYTICS_ENDPOINT_DEV = 'http://127.0.0.1:8000/';
 }
