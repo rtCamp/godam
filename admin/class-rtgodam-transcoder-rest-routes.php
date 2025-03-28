5c008f1091e9cf679485bd8cdc63f8e7e05bd42e
KMchaudhary <kuldipkumar.chaudhary@rtcamp.com>
2025-03-28 15:51:51 +0530

    Add get_callback_ulr function

--- admin/class-rtgodam-transcoder-rest-routes.php
+++ admin/class-rtgodam-transcoder-rest-routes.php
@@ -14,18 +14,11 @@ defined( 'ABSPATH' ) || exit;
 class RTGODAM_Transcoder_Rest_Routes extends WP_REST_Controller {
 
 	/**
-	 * Version of REST route.
-	 *
-	 * @var int
-	 */
-	public $version = 1;
-
-	/**
 	 * Prefix for API endpoint namespace.
 	 *
 	 * @var string
 	 */
-	public $namespace_prefix = 'transcoder/v';
+	public $namespace_prefix = 'godam/v1';
 
 	/**
 	 * RT Transcoder Handler object.
@@ -57,7 +50,7 @@ class RTGODAM_Transcoder_Rest_Routes extends WP_REST_Controller {
 
 		// Register `transcoder-callback` route to handle callback request by the FFMPEG transcoding server.
 		register_rest_route(
-			$this->namespace_prefix . $this->version,
+			$this->namespace_prefix,
 			'/transcoder-callback',
 			array(
 				'methods'             => WP_REST_Server::CREATABLE,
@@ -132,6 +125,15 @@ class RTGODAM_Transcoder_Rest_Routes extends WP_REST_Controller {
 				),
 			)
 		);
+	}
+
+	/**
+	 * Return the callback URL for the transcoder.
+	 *
+	 * @return string
+	 */
+	public function get_callback_url() {
+		return rest_url( $this->namespace_prefix . $this->version . '/transcoder-callback' );
 	}
 
 	/**
