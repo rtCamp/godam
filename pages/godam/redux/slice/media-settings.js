f0f0604fe9e971002ef6fc67d4b922d5e948d31c
Juzar Bharmal <53657281+Juzar10@users.noreply.github.com>
2025-04-02 17:04:44 +0530

    Refactor Settings Page: Redux API Query Integration & UI Redesign [develop] (#249)
    * feat: add initial styles for the new theme
    
    * feat: add App.js general setting changes
    
    * fix: save settings fixed for the general settings
    
    * feat: add the verify API settings
    
    * feat: add video settings redesign
    
    * fix: media library enable vs diable changes
    
    * fix: eslint and styling issue along with phpcs issues
    
    * feat: remove unused ImageSettings
    
    * feat: add the premium feature upgrade message
    
    * feat: update the lintstage file to include the .jsx files
    
    * fix: phpcs issues
    
    * feat: rename license reference to the API

---
+++ pages/godam/redux/slice/media-settings.js
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+	video: {
+		video_quality: [],
+		video_thumbnails: 5,
+		overwrite_thumbnails: false,
+		watermark: false,
+		use_watermark_image: false,
+		watermark_text: '',
+		watermark_url: '',
+	},
+	general: {
+		enable_folder_organization: true,
+		brand_color: '#000000',
+		brand_image: '',
+	},
+};
+
+const mediaSettingsSlice = createSlice( {
+	name: 'mediaSettings',
+	initialState,
+	reducers: {
+		// Sets the entire media settings (validates allowed keys)
+		setMediaSettings: ( state, action ) => {
+			Object.keys( action.payload ).forEach( ( category ) => {
+				if ( state[ category ] ) {
+					Object.keys( action.payload[ category ] ).forEach( ( key ) => {
+						if ( key in state[ category ] ) {
+							state[ category ][ key ] = action.payload[ category ][ key ];
+						}
+					} );
+				}
+			} );
+		},
+
+		// Updates a specific setting dynamically
+		updateMediaSetting: ( state, action ) => {
+			const { category, key, value } = action.payload; // e.g., { category: 'video', key: 'video_format', value: 'mp4' }
+
+			if ( state[ category ] && key in state[ category ] ) {
+				state[ category ][ key ] = value;
+			}
+		},
+	},
+} );
+
+export const { setMediaSettings, updateMediaSetting } = mediaSettingsSlice.actions;
+export default mediaSettingsSlice.reducer;
