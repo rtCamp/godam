4bfcabc54e1494edb1eebe31d7a5625bb844d262
KMchaudhary <kuldipkumar.chaudhary@rtcamp.com>
2025-04-10 15:39:45 +0530

    Remove console.log statements

--- pages/video-editor/VideoJSPlayer.js
+++ pages/video-editor/VideoJSPlayer.js
@@ -77,9 +77,6 @@ export const VideoJS = ( props ) => {
 			videoElement.classList.add( 'vjs-big-play-centered' );
 			videoRef.current.appendChild( videoElement );
 
-			console.log( 'options', options);
-			
-
 			const player = ( playerRef.current = videojs( videoElement, options, () => {
 				videojs.log( 'player is ready' );
 				onReady && onReady( player );
