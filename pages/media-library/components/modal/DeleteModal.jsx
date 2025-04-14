1fadf143767aa5310071a1a0b004aa338b93da68
Juzar Bharmal <53657281+Juzar10@users.noreply.github.com>
2025-04-14 20:44:54 +0530

    Enhance focus handling and refactor button group in modal Component (#267)
    * fix: enhance focus handling and refactor button group in modal components
    
    * fix: add loading state management to modal components for folder deletion, creation, and renaming

--- pages/media-library/components/modal/DeleteModal.jsx
+++ pages/media-library/components/modal/DeleteModal.jsx
@@ -1,13 +1,13 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 /**
  * WordPress dependencies
  */
-import { Button, ButtonGroup, Modal } from '@wordpress/components';
+import { Button, Modal } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -18,14 +18,33 @@ import './scss/modal.scss';
 import { triggerFilterChange } from '../../data/media-grid';
 
 const DeleteModal = () => {
+	const [ isLoading, setIsLoading ] = useState( false );
+
 	const dispatch = useDispatch();
 
 	const isOpen = useSelector( ( state ) => state.FolderReducer.modals.delete );
 	const selectedFolder = useSelector( ( state ) => state.FolderReducer.selectedFolder );
 
 	const [ deleteFolderMutation ] = useDeleteFolderMutation();
+	const ref = useRef( null );
+
+	useEffect( () => {
+		if ( isOpen ) {
+			setIsLoading( false );
+			// Focus the button after render
+			setTimeout( () => {
+				ref.current?.focus();
+			}, 0 );
+		}
+	}, [ isOpen ] );
 
 	const handleSubmit = async () => {
+		if ( isLoading ) {
+			return;
+		}
+
+		setIsLoading( true );
+
 		try {
 			await deleteFolderMutation( selectedFolder.id );
 
@@ -46,9 +65,16 @@ const DeleteModal = () => {
 					type: 'error',
 				},
 			) );
+		} finally {
+			setIsLoading( false );
+			dispatch( closeModal( 'delete' ) );
 		}
+	};
 
-		dispatch( closeModal( 'delete' ) );
+	const handleKeyDown = ( e ) => {
+		if ( e.key === 'Enter' ) {
+			handleSubmit();
+		}
 	};
 
 	return (
@@ -65,19 +91,22 @@ const DeleteModal = () => {
 					Are you sure you want to proceed? This action cannot be undone.
 				</p>
 
-				<ButtonGroup className="modal__button-group">
+				<div className="modal__button-group">
 					<Button
+						isBusy={ isLoading }
+						ref={ ref }
 						text="Delete"
 						variant="primary"
 						onClick={ () => handleSubmit() }
 						isDestructive
+						onKeyDown={ handleKeyDown }
 					/>
 					<Button
 						text="Cancel"
 						onClick={ () => dispatch( closeModal( 'delete' ) ) }
 						isDestructive
 					/>
-				</ButtonGroup>
+				</div>
 			</Modal>
 		)
 	);
