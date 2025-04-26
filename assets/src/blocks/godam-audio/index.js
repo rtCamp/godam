d47a57c840eee3b3a30f21812b7cfb469ff12f6a
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2025-04-26 19:09:27 +0530

    Feat: Create GoDAM Audio block

---
+++ assets/src/blocks/godam-audio/index.js
@@ -0,0 +1,32 @@
+/**
+ * WordPress dependencies
+ */
+import { audio as icon } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import initBlock from '../../utils/init-block';
+import edit from './edit';
+import metadata from './block.json';
+import save from './save';
+
+const { name } = metadata;
+
+export { metadata, name };
+
+export const settings = {
+	icon,
+	example: {
+		attributes: {
+			src: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Armstrong_Small_Step.ogg',
+		},
+		viewportWidth: 350,
+	},
+	edit,
+	save,
+};
+
+export const init = () => initBlock( { name, metadata, settings } );
+
+init();
