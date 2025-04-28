c6ae596118d1c4f501b28c4fd9e2ba394a558c12
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2025-04-28 11:18:23 +0530

    feat: Add dynamic rendering for GoDAM Audio block using CDN URL

--- assets/src/blocks/godam-audio/save.js
+++ assets/src/blocks/godam-audio/save.js
@@ -1,32 +1,3 @@
-/**
- * WordPress dependencies
- */
-import {
-	RichText,
-	useBlockProps,
-} from '@wordpress/block-editor';
-
-export default function save( { attributes } ) {
-	const { autoplay, caption, loop, preload, src } = attributes;
-
-	return (
-		src && (
-			<figure { ...useBlockProps.save() }>
-				<audio
-					controls="controls"
-					src={ src }
-					autoPlay={ autoplay }
-					loop={ loop }
-					preload={ preload }
-				/>
-				{ ! RichText.isEmpty( caption ) && (
-					<RichText.Content
-						tagName="figcaption"
-						value={ caption }
-						className="caption"
-					/>
-				) }
-			</figure>
-		)
-	);
+export default function save() {
+	return null;
 }
