b34d892ff4a4f8473a735a9eba55949ec6422ede
Prachi Garg <72646230+prachigarg19@users.noreply.github.com>
2025-05-22 16:25:02 +0530

    Fix share button style (#323)
    * feat: fix share button style
    
    * refactor: fix indentation
    
    * refactor: fix css indentation

--- assets/src/blocks/godam-player/frontend.js
+++ assets/src/blocks/godam-player/frontend.js
@@ -265,9 +265,8 @@ function GODAMPlayer( videoRef = null ) {
 		const Button = videojs.getComponent( 'Button' );
 
 		class GodamShareButton extends Button {
-			constructor( p, options ) {
-				super( p, options );
-				this.controlText( 'Share' );
+			buildCSSClass() {
+				return `godam-share-button ${ super.buildCSSClass() }`;
 			}
 
 			// Set the button content
@@ -427,13 +426,15 @@ function GODAMPlayer( videoRef = null ) {
 		player.ready( function() {
 			player.jobId = video.dataset.job_id; // Store the result when it's available
 
-			const controlBar = player.getChild( 'controlBar' );
-			if ( controlBar && player.jobId !== '' ) {
-				controlBar.addChild(
-					'GodamShareButton',
-					{},
-					controlBar.children().length - 1,
+			const videoContainer = player.el().closest( '.easydam-video-container' );
+			if ( videoContainer && player.jobId !== '' ) {
+				const shareButton = new GodamShareButton( player );
+				const buttonEl = shareButton.createEl();
+				buttonEl.addEventListener(
+					'click',
+					shareButton.handleClick.bind( shareButton ),
 				);
+				videoContainer.appendChild( buttonEl );
 			}
 		} );
 
