ca6b9a0bf6cee032d69a73bcc7dcc4be5d4a2c0e
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2025-05-25 22:43:17 +0530

    fix: Errors while fetching transcoding status

--- assets/src/js/media-library/transcoding-status/transcoding-checker.js
+++ assets/src/js/media-library/transcoding-status/transcoding-checker.js
@@ -14,6 +14,9 @@ class TranscodingChecker {
 
 	async fetchStatusOnce( postIds ) {
 		const url = this.buildUrl( postIds );
+		if ( ! url ) {
+			return;
+		}
 		try {
 			const response = await fetch( url );
 			const data = await response.json();
@@ -24,9 +27,13 @@ class TranscodingChecker {
 	}
 
 	async fetchStatus() {
-		try {
-			const response = await fetch( this.buildUrl( this.postIds ) );
+		const url = this.buildUrl( this.postIds );
+		if ( ! url ) {
+			return;
+		}
 
+		try {
+			const response = await fetch( url );
 			const data = await response.json();
 
 			this.updateCallback( data );
@@ -48,7 +55,12 @@ class TranscodingChecker {
 	}
 
 	buildUrl( postIds ) {
-		const queryString = postIds.map( ( id ) => `ids[]=${ id }` ).join( '&' );
+		const validIds = postIds.filter( ( id ) => Number.isInteger( id ) && id > 0 );
+		if ( validIds.length === 0 ) {
+			return null;
+		}
+
+		const queryString = validIds.map( ( id ) => `ids[]=${ id }` ).join( '&' );
 		return `${ this.apiUrl }?${ queryString }`;
 	}
 
