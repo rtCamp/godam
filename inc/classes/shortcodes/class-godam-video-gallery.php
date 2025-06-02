b6067a778ab4100cf372db665230bb6857d9cfa1
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2025-06-02 11:45:34 +0530

    feat: Disable GoDAM Video Gallery shortcode if no API Key present

--- inc/classes/shortcodes/class-godam-video-gallery.php
+++ inc/classes/shortcodes/class-godam-video-gallery.php
@@ -81,6 +81,12 @@ class GoDAM_Video_Gallery {
 
 		wp_enqueue_style( 'godam-gallery-style' );
 
+		// Check for API key.
+		$api_key = get_option( 'rtgodam-api-key', '' );
+		if ( empty( $api_key ) ) {
+			return '<div class="godam-video-gallery-error">' . esc_html__( '[API Key missing. Please configure the API key in the GoDAM settings.]', 'godam' ) . '</div>';
+		}
+
 		$args = array(
 			'post_type'      => 'attachment',
 			'post_status'    => 'inherit',
