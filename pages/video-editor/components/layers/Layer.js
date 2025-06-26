5db0b26884d2f63751310bb040700c6f5d310908
hbhalodia <hit.bhalodia@rtcamp.com>
2025-06-26 15:30:17 +0530

    GH 543: Optimize the Layer component to use object based structuring and render component
    Updated the `Layer` component to use object based structuring, removed individual component to render based on the layer type, updated to fetch it from the object instead

--- pages/video-editor/components/layers/Layer.js
+++ pages/video-editor/components/layers/Layer.js
@@ -12,29 +12,44 @@ import Ads from './AdsLayer';
 import PollLayer from './PollLayer';
 import WoocommerceLayer from './WoocommerceLayer';
 
+/**
+ * Layer Component that needs to be used.
+ */
+const LayerComponents = {
+	form: {
+		component: FormLayer,
+	},
+	cta: {
+		component: CTALayer,
+	},
+	hotspot: {
+		component: HotspotLayer,
+	},
+	ad: {
+		component: Ads,
+	},
+	poll: {
+		component: PollLayer,
+	},
+	woo: {
+		component: WoocommerceLayer,
+	},
+};
+
+/**
+ * Component to add the layer based on the type.
+ *
+ * @param {Object}   param0          - Props for the Layer component.
+ * @param {Object}   param0.layer    - The layer data containing type and associated metadata.
+ * @param {Function} param0.goBack   - Callback to navigate back to the previous step.
+ * @param {number}   param0.duration - Duration of the video in seconds or milliseconds.
+ *
+ * @return {JSX.Element} The rendered Layer component.
+ */
 const Layer = ( { layer, goBack, duration } ) => {
-	return (
-		<>
-			{
-				layer.type === 'form' && <FormLayer layerID={ layer.id } goBack={ goBack } duration={ duration } />
-			}
-			{
-				layer.type === 'cta' && <CTALayer layerID={ layer.id } goBack={ goBack } duration={ duration } />
-			}
-			{
-				layer.type === 'hotspot' && <HotspotLayer layerID={ layer.id } goBack={ goBack } duration={ duration } />
-			}
-			{
-				layer.type === 'ad' && <Ads layerID={ layer.id } goBack={ goBack } duration={ duration } />
-			}
-			{
-				layer.type === 'poll' && <PollLayer layerID={ layer.id } goBack={ goBack } duration={ duration } />
-			}
-			{
-				layer.type === 'woo' && <WoocommerceLayer layerID={ layer.id } goBack={ goBack } />
-			}
-		</>
-	);
+	const Component = LayerComponents[ layer?.type ?? 'cta' ]?.component;
+
+	return <Component layerID={ layer.id } goBack={ goBack } duration={ duration } />;
 };
 
 export default Layer;
