03326c1b3f53daf79ec6bd3edc1d04582c34fba5
Vipul Patil <65603015+patil-vipul@users.noreply.github.com>
2025-06-27 13:51:17 +0530

    FEAT: Add functionality to copy GoDAM Video block (#554)
    * FEAT: Add `Copy Video Block` functionality.
    
    Copies GoDAM Video block to clipboard.
    
    * FEAT: Add utility functions required to copy GoDAM block.
    
    * FIX: Align Overlay Menu items to the left.

--- pages/video-editor/VideoEditor.js
+++ pages/video-editor/VideoEditor.js
@@ -9,7 +9,7 @@ import { useSelector, useDispatch } from 'react-redux';
  */
 import { Button, TabPanel, Snackbar } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { seen } from '@wordpress/icons';
+import { copy, seen } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -30,12 +30,15 @@ import './video-editor.scss';
 import { useGetAttachmentMetaQuery, useSaveAttachmentMetaMutation } from './redux/api/attachment';
 import { useFetchForms } from './components/forms/fetchForms';
 import Chapters from './components/chapters/Chapters';
+import { copyGoDAMVideoBlock } from './utils/index';
 
 const VideoEditor = ( { attachmentID } ) => {
 	const [ currentTime, setCurrentTime ] = useState( 0 );
 	const [ showSaveMessage, setShowSaveMessage ] = useState( false );
 	const [ sources, setSources ] = useState( [] );
 	const [ duration, setDuration ] = useState( 0 );
+	const [ snackbarMessage, setSnackbarMessage ] = useState( '' );
+	const [ showSnackbar, setShowSnackbar ] = useState( false );
 
 	const playerRef = useRef( null );
 
@@ -184,6 +187,21 @@ const VideoEditor = ( { attachmentID } ) => {
 		return `${ minsStr }:${ secsStr }`;
 	};
 
+	const handleCopyGoDAMVideoBlock = async () => {
+		const result = await copyGoDAMVideoBlock( attachmentID );
+		if ( result ) {
+			setSnackbarMessage( __( 'GoDAM Video Block copied to clipboard', 'godam' ) );
+			setShowSnackbar( true );
+		} else {
+			setSnackbarMessage( __( 'Failed to copy GoDAM Video Block', 'godam' ) );
+			setShowSnackbar( true );
+		}
+	};
+
+	const handleOnSnackbarRemove = () => {
+		setShowSnackbar( false );
+	};
+
 	const tabConfig = [
 		{
 			name: 'layers',
@@ -292,8 +310,26 @@ const VideoEditor = ( { attachmentID } ) => {
 						)
 					}
 
+					{ showSnackbar && (
+						<Snackbar className="absolute bottom-4 right-4 opacity-70 z-50"
+							onRemove={ handleOnSnackbarRemove }
+						>
+							{ snackbarMessage }
+						</Snackbar>
+					) }
+
 					<div className="absolute top-4 left-4 right-4">
-						<div className="flex justify-end items-center">
+						<div className="flex space-x-2 justify-end items-center">
+							<Button
+								variant="secondary"
+								icon={ copy }
+								iconPosition="left"
+								onClick={ handleCopyGoDAMVideoBlock }
+								size="compact"
+								className="godam-button"
+							>
+								{ __( 'Copy Block', 'godam' ) }
+							</Button>
 							<Button
 								variant="secondary"
 								href={ `/?godam_page=video-preview&id=${ attachmentID }` }
