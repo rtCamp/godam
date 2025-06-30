6ffc414d4407e8cc26885cc4af6f9b5def3ac7b0
hbhalodia <hit.bhalodia@rtcamp.com>
2025-06-30 11:38:51 +0530

    GH 567: Add the edit component inspector control for recorder field srfm godam

--- assets/src/blocks/sureforms/blocks/recorder/edit.js
+++ assets/src/blocks/sureforms/blocks/recorder/edit.js
@@ -1,3 +1,218 @@
-export default function Edit() {
-	return <div>Recorder Field</div>;
+/**
+ * External dependencies
+ */
+import clsx from 'clsx';
+
+/**
+ * WordPress dependencies
+ */
+import { useEffect } from '@wordpress/element';
+import {
+	CheckboxControl,
+	TextControl,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalText as Text,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalNumberControl as NumberControl,
+	ToggleControl,
+	PanelBody,
+} from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import {
+	useGetCurrentFormId,
+	useErrMessage,
+	getMaxFileSize,
+} from '../../utils';
+
+/**
+ * Edit function for the block.
+ *
+ * @param {Object} props The Component props.
+ */
+export default function Edit( props ) {
+	const { clientId, attributes, setAttributes, className } = props;
+	const {
+		label,
+		description,
+		formId,
+		required,
+		fileSelector,
+		maxFileSize,
+		errorMsg,
+	} = attributes;
+	const currentFormId = useGetCurrentFormId( clientId );
+
+	const classes = clsx( className, {
+		'godam-srfm-recorder': true,
+	} );
+
+	const blockProps = useBlockProps( {
+		className: classes,
+	} );
+	const maxFileSizeLimit = getMaxFileSize();
+
+	useEffect( () => {
+		if ( formId !== currentFormId ) {
+			setAttributes( { formId: currentFormId } );
+		}
+	}, [ formId, setAttributes, currentFormId ] );
+
+	const {
+		// eslint-disable-next-line no-unused-vars
+		currentMessage: currentErrorMsg,
+		setCurrentMessage: setCurrentErrorMsg,
+	} = useErrMessage( errorMsg );
+
+	/**
+	 * File selector option.
+	 *
+	 * @param {string} value
+	 */
+	const updateFileSelector = ( value ) => {
+		let updated = [];
+
+		if ( fileSelector.includes( value ) ) {
+			updated = fileSelector.filter( ( item ) => item !== value );
+		} else {
+			updated = [ ...fileSelector, value ];
+		}
+		setAttributes( { fileSelector: updated } );
+	};
+
+	/**
+	 * Check for file selector.
+	 *
+	 * @param {string} value
+	 */
+	const checkFileSelector = ( value ) => {
+		return fileSelector.includes( value );
+	};
+
+	/**
+	 * Inspector Control options.
+	 */
+	const fieldOptions = [
+		{
+			id: 'label',
+			component: (
+				<TextControl
+					__next40pxDefaultSize
+					label={ __( 'Label', 'godam' ) }
+					help={ __( 'Enter label for the field', 'godam' ) }
+					onChange={ ( value ) => {
+						setAttributes( { label: value } );
+					} }
+					value={ label }
+				/>
+			),
+		},
+		{
+			id: 'description',
+			component: (
+				<TextControl
+					__next40pxDefaultSize
+					label={ __( 'Description', 'godam' ) }
+					help={ __( 'Enter description for the field', 'godam' ) }
+					onChange={ ( value ) => {
+						setAttributes( { description: value } );
+					} }
+					value={ description }
+				/>
+			),
+		},
+		{
+			id: 'file-selector',
+			component: (
+				<>
+					<Text isBlock style={ { marginBottom: '20px' } }>
+						{ __( 'Choose file selector', 'godam' ) }
+					</Text>
+					<CheckboxControl
+						__nextHasNoMarginBottom
+						label={ __( 'Local files', 'godam' ) }
+						checked={ checkFileSelector( 'local' ) }
+						onChange={ () => updateFileSelector( 'local' ) }
+					/>
+					<CheckboxControl
+						__nextHasNoMarginBottom
+						label={ __( 'Webcam', 'godam' ) }
+						checked={ checkFileSelector( 'webcam' ) }
+						onChange={ () => updateFileSelector( 'webcam' ) }
+					/>
+					<CheckboxControl
+						__nextHasNoMarginBottom
+						label={ __( 'Screencast', 'godam' ) }
+						checked={ checkFileSelector( 'screencast' ) }
+						onChange={ () => updateFileSelector( 'screencast' ) }
+					/>
+				</>
+			),
+		},
+		{
+			id: 'max-file-size',
+			component: (
+				<NumberControl
+					__next40pxDefaultSize
+					__nextHasNoMarginBottom
+					label={ __( 'Maximum file size (in MB)', 'godam' ) }
+					help={ sprintf(
+						// Translators: %s will be replaced with the maximum file upload size allowed on the server (e.g., "300MB").
+						__( 'Maximum allowed on this server: %s MB', 'godam' ),
+						maxFileSizeLimit,
+					) }
+					onChange={ ( value ) => {
+						setAttributes( { maxFileSize: parseInt( value, 10 ) } );
+					} }
+					max={ maxFileSizeLimit }
+					min={ 10 }
+					value={ parseInt( maxFileSize, 10 ) }
+				/>
+			),
+		},
+		{
+			id: 'required',
+			component: (
+				<ToggleControl
+					checked={ required }
+					onChange={ () => setAttributes( { required: ! required } ) }
+				/>
+			),
+		},
+		{
+			id: 'error-message',
+			component: required ? (
+				<TextControl
+					label={ __( 'Error Message' ) }
+					__next40pxDefaultSize
+					__nextHasNoMarginBottom
+					help={ __( 'Enter the error message', 'godam' ) }
+					value={ currentErrorMsg }
+					onChange={ ( value ) => {
+						setCurrentErrorMsg( value );
+						setAttributes( { errorMsg: value } );
+					} }
+				/>
+			) : null,
+		},
+	];
+
+	return (
+		<>
+			<div { ...blockProps }>
+				<InspectorControls>
+					<PanelBody title={ __( 'Field settings', 'godam' ) }>
+						{ fieldOptions.map( ( option ) => option.component ) }
+					</PanelBody>
+				</InspectorControls>
+				<>
+					Button
+				</>
+			</div>
+		</>
+	);
 }
---
+++ assets/src/blocks/sureforms/utils.js
@@ -0,0 +1,48 @@
+/**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+import { useState, useEffect } from '@wordpress/element';
+
+export const useGetCurrentFormId = ( clientId ) => {
+	return useSelect( ( select ) => {
+		// parent block id attribute.
+		const parents = select( blockEditorStore ).getBlockParents( clientId );
+		const parentBlock = select( blockEditorStore ).getBlocksByClientId(
+			parents?.[ 0 ],
+		);
+		// current post id.
+		const postId = select( 'core/editor' ).getCurrentPostId();
+		return parentBlock?.[ 0 ]?.attributes?.id || postId;
+	} );
+};
+
+/**
+ * Generate Required Error Message.
+ *
+ * @param {string} message Custom error message.
+ * @return {Object} currentErrorMsg, setCurrentErrorMsg, currentUniqueMessage, setCurrentUniqueMessage.
+ */
+export const useErrMessage = ( message ) => {
+	const [ currentMessage, setCurrentMessage ] = useState();
+
+	useEffect( () => {
+		setCurrentMessage( message );
+	}, [ message ] );
+
+	return { currentMessage, setCurrentMessage };
+};
+
+/**
+ * Get Max file size for the site.
+ */
+export function getMaxFileSize() {
+	const maxFileSizeUpload =
+	window?._wpPluploadSettings?.defaults?.filters?.max_file_size;
+
+	const numericPart = maxFileSizeUpload.replace( '/b$/i', '' );
+	const sizeInBytes = parseInt( numericPart, 10 );
+
+	return sizeInBytes / ( 1024 * 1024 );
+}
