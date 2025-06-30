54fcbb744358ba69df303b908a1c99806292f5d2
Subodh Rajpopat <58080782+subodhr258@users.noreply.github.com>
2025-06-30 11:26:40 +0530

    fix: Middleware errors for analytics on dev environment (#572)
    * fix: Middleware error on Dashboard page
    
    * fix: Middleware error on Analytics page

--- pages/analytics/redux/store.js
+++ pages/analytics/redux/store.js
@@ -6,11 +6,16 @@ import { configureStore } from '@reduxjs/toolkit';
  * Internal dependencies
  */
 import { analyticsApi } from './api/analyticsApi';
+import { dashboardAnalyticsApi } from '../../dashboard/redux/api/dashboardAnalyticsApi';
 
 export default configureStore( {
 	reducer: {
 		[ analyticsApi.reducerPath ]: analyticsApi.reducer,
+		[ dashboardAnalyticsApi.reducerPath ]: dashboardAnalyticsApi.reducer,
 	},
 	middleware: ( getDefaultMiddleware ) =>
-		getDefaultMiddleware().concat( analyticsApi.middleware ),
+		getDefaultMiddleware().concat(
+			analyticsApi.middleware,
+			dashboardAnalyticsApi.middleware,
+		),
 } );
