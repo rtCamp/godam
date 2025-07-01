5f776782081dc9267cdf19d67898b76fc2605e9d
Vipul Patil <vipulrpatil.8@gmail.com>
2025-07-01 23:58:18 +0530

    FIX: Register separate taxonomy for Media Tag.

--- inc/classes/taxonomies/class-media-tag.php
+++ inc/classes/taxonomies/class-media-tag.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Adds default WordPress `post_tag` taxonomy to media library (attachments).
+ * Register `media_tag` taxonomy for the media attachments.
  *
  * @package GoDAM
  */
@@ -9,41 +9,81 @@ namespace RTGODAM\Inc\Taxonomies;
 
 defined( 'ABSPATH' ) || exit;
 
-use RTGODAM\Inc\Traits\Singleton;
-
 /**
  * Class Media_Tag
  */
-class Media_Tag {
-
-	use Singleton;
+class Media_Tag extends Base {
 
 	/**
-	 * Constructor.
-	 */
-	protected function __construct() {
-		$this->setup_hooks();
-	}
-
-	/**
-	 * Setup hooks.
-	 */
-	protected function setup_hooks() {
-		add_action( 'init', array( $this, 'register_tag_for_media' ) );
-		add_action( 'restrict_manage_posts', array( $this, 'add_tag_filter_dropdown' ) );
-		add_filter( 'manage_media_columns', array( $this, 'manage_tag_columns' ) );
-		add_filter( 'parse_query', array( $this, 'filter_media_by_tag' ) );
-	}
-
-	/**
-	 * Register tag taxonomy for `attachment` post type.
+	 * Slug of taxonomy.
 	 *
-	 * Enables tags to be assigned to media attachments in the media library.
+	 * @var string
+	 */
+	const SLUG = 'media_tag';
+
+	/**
+	 * To setup action/filter.
 	 *
 	 * @return void
 	 */
-	public function register_tag_for_media() {
-		register_taxonomy_for_object_type( 'post_tag', 'attachment' );
+	protected function setup_hooks() {
+		parent::setup_hooks();
+
+		add_action( 'restrict_manage_posts', array( $this, 'add_tag_filter_dropdown' ) );
+	}
+
+	/**
+	 * Labels for taxonomy.
+	 *
+	 * @return array
+	 */
+	public function get_labels(): array {
+
+		return array(
+			'name'              => _x( 'Tags', 'Tag', 'godam' ),
+			'singular_name'     => _x( 'Tag', 'Tag', 'godam' ),
+			'search_items'      => __( 'Search Tags', 'godam' ),
+			'all_items'         => __( 'All Tags', 'godam' ),
+			'parent_item'       => __( 'Parent Folder', 'godam' ),
+			'parent_item_colon' => __( 'Parent Folder:', 'godam' ),
+			'edit_item'         => __( 'Edit Tag', 'godam' ),
+			'update_item'       => __( 'Update Tag', 'godam' ),
+			'add_new_item'      => __( 'Add New Tag', 'godam' ),
+			'new_item_name'     => __( 'New Tag Name', 'godam' ),
+			'menu_name'         => __( 'Tags', 'godam' ),
+		);
+	}
+
+	/**
+	 * List of post types for taxonomy.
+	 *
+	 * @return array
+	 */
+	public function get_post_types(): array {
+
+		return array(
+			'attachment',
+		);
+	}
+
+	/**
+	 * To get argument to register taxonomy.
+	 *
+	 * @return array
+	 */
+	public function get_args(): array {
+
+		$args = parent::get_args();
+
+		$extra = array(
+			'hierarchical'      => false,
+			'show_ui'           => true,
+			'show_admin_column' => false,
+			'show_in_rest'      => true,
+			'query_var'         => true,
+		);
+
+		return array_merge( $args, $extra );
 	}
 
 	/**
@@ -62,68 +102,25 @@ class Media_Tag {
 			return;
 		}
 
-		$taxonomy = 'post_tag';
+		$taxonomy = 'media_tag';
 		$selected = isset( $_GET[ $taxonomy ] ) ? sanitize_text_field( wp_unslash( $_GET[ $taxonomy ] ) ) : ''; // phpcs:ignore WordPress.Security.NonceVerification.Recommended -- The media filter form has nonce verification, so this is safe to use without additional nonce checks.
 
+		$has_terms = wp_count_terms( array( 'taxonomy' => $taxonomy ) );
+
+		$dropdown_args = array(
+			'show_option_all' => __( 'All Tags', 'godam' ),
+			'taxonomy'        => $taxonomy,
+			'name'            => $taxonomy,
+			'orderby'         => 'name',
+			'selected'        => $selected,
+			'value_field'     => 'slug',
+			'depth'           => 3,
+			'hide_empty'      => false,
+		);
+
 		wp_dropdown_categories(
-			array(
-				'show_option_all' => __( 'All Tags', 'godam' ),
-				'taxonomy'        => $taxonomy,
-				'name'            => $taxonomy,
-				'orderby'         => 'name',
-				'selected'        => $selected,
-				'depth'           => 3,
-				'hide_empty'      => false,
-			)
+			$has_terms ? $dropdown_args : array_merge( $dropdown_args, array( 'show_option_none' => __( 'No Tags', 'godam' ) ) )
 		);
 		// phpcs:enable WordPress.Security.NonceVerification.Recommended
-	}
-
-	/**
-	 * Filter media library query by selected tag.
-	 *
-	 * Modifies the WordPress query to filter media attachments based on
-	 * the selected tag from the dropdown filter.
-	 *
-	 * @param WP_Query $query The WordPress query object.
-	 *
-	 * @return WP_Query Modified query object.
-	 */
-	public function filter_media_by_tag( $query ) {
-		// phpcs:disable WordPress.Security.NonceVerification.Recommended -- The media filter form has nonce verification, so this is safe to use without additional nonce checks.
-		global $pagenow;
-
-		if ( ! is_admin() || 'upload.php' !== $pagenow ) {
-			return $query;
-		}
-
-		$taxonomy = 'post_tag';
-
-		if ( isset( $_GET[ $taxonomy ] ) && ! empty( $_GET[ $taxonomy ] ) ) {
-			$term                           = sanitize_text_field( wp_unslash( $_GET[ $taxonomy ] ) );
-			$query->query_vars['tax_query'] = array( // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_tax_query -- This is a standard query modification for filtering media by tag.
-				array(
-					'taxonomy' => $taxonomy,
-					'field'    => 'term_id',
-					'terms'    => $term,
-				),
-			);
-		}
-
-		return $query;
-		// phpcs:enable WordPress.Security.NonceVerification.Recommended -- The media filter form has nonce verification, so this is safe to use without additional nonce checks.
-	}
-
-	/**
-	 * Hide the `tags` column created after registering `post_tag` taxonomy.
-	 *
-	 * @param Array $columns  of columns in the media library.
-	 *
-	 * @return mixed
-	 */
-	public function manage_tag_columns( array $columns ) {
-		unset( $columns['tags'] );
-
-		return $columns;
 	}
 }
