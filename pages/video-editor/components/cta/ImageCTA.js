fb136b61a5318bc283f88f4f6c5ae16e6a7ff805
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2025-07-02 20:21:11 +0530

    fix: Image transparency in CTA

--- pages/video-editor/components/cta/ImageCTA.js
+++ pages/video-editor/components/cta/ImageCTA.js
@@ -217,12 +217,12 @@ const ImageCTA = ( { layerID } ) => {
 						__nextHasNoMarginBottom
 						__next40pxDefaultSize
 						help={ __( 'Please select how transparent you would like this.', 'godam' ) }
-						initialPosition={ 0 }
+						initialPosition={ 1 }
 						max={ 1 }
 						min={ 0 }
 						onChange={ ( value ) => updateField( 'imageOpacity', value ) }
 						step={ 0.1 }
-						value={ layer.imageOpacity }
+						value={ layer.imageOpacity ?? 1 }
 						label={ __( 'Opacity of background image', 'godam' ) }
 					/>
 				</div>
