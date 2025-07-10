f4ea90e4f99007ceda372cc79ac3d5642fb65f17
Vipul Patil <vipulrpatil.8@gmail.com>
2025-07-10 16:12:10 +0530

    FEAT: Add support for `.flv` video format to frontend player.

--- inc/templates/godam-player.php
+++ inc/templates/godam-player.php
@@ -127,6 +127,13 @@ $video_setup = array(
 	'preload'     => $preload,
 	'poster'      => empty( $poster ) ? $poster_image : $poster,
 	'fluid'       => true,
+	'flvjs'       => array(
+		'mediaDataSource' => array(
+			'isLive'          => true,
+			'cors'            => false,
+			'withCredentials' => false,
+		),
+	),
 	'sources'     => $sources,
 	'playsinline' => true,
 	'controlBar'  => array(
@@ -269,7 +276,7 @@ if ( $is_shortcode || $is_elementor_widget ) {
 						<?php echo wp_kses_post( $heading ); ?>
 					</div>
 				<?php endif; ?>
-					
+
 			<div class="animate-play-btn">
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
 						<path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
