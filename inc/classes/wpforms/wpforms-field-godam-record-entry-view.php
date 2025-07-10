4abb8900e8a142c5524c48c720c9ff10fc290539
Sagar Tamang <mi5t4n@gmail.com>
2025-07-10 15:54:45 +0545

    populate attachment name in the entry view page

--- inc/classes/wpforms/wpforms-field-godam-record-entry-view.php
+++ inc/classes/wpforms/wpforms-field-godam-record-entry-view.php
@@ -12,8 +12,15 @@
 <div class="godam-video-preview">
 	<?php
 		// No need to escape here, the entire template will be returned as strings,
-		// which will be later on escaped using wp_kses_post() by WPForms before rendering the field
+		// which will be later on escaped using wp_kses_post() by WPForms before rendering the field.
 		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		echo force_balance_tags( do_shortcode( "[godam_video poster='{$thumbnail_url}' src='{$attachment_url}' transcoded_url='{$transcoded_url}']" ) );
 	?>
+	<a
+		href="<?php echo esc_url( $attachment_url ); ?>"
+		target="_blank"
+		class="godam-video-link <?php echo ( empty( $value ) ? 'hidden' : '' ); ?>"
+	>
+		<div class="godam-video-name"><?php echo esc_html( $attachment_name ); ?></div>
+	</a>
 </div>
