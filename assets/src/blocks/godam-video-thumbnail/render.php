3daaa28f44532823f54f25c855ee098d2a8da1c6
yashjawale <yash25.j@gmail.com>
2025-07-11 13:42:10 +0530

    feat: add thumbnail display on frontend

--- assets/src/blocks/godam-video-thumbnail/render.php
+++ assets/src/blocks/godam-video-thumbnail/render.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Render template for the GoDAM Audio Block.
+ * Render template for the GoDAM Video Thumbnail Block.
  *
  * @package GoDAM
  */
@@ -9,7 +9,40 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+$video_post_id = get_the_ID();
+$thumbnail_url = '';
+$alt_text      = '';
+
+if ( $video_post_id ) {
+
+	// Get attachment ID from post meta.
+	$attachment_id = get_post_meta( $video_post_id, '_godam_attachment_id', true );
+
+	// Get thumbnail URL directly from attachment's meta.
+	$thumbnail_url = get_post_meta( $video_post_id, '_godam_video_thumbnail_url', true );
+
+	// Set alt text to the post title.
+	$alt_text = get_the_title();
+
+}
+
+$wrapper_classes = 'godam-video-thumbnail__container';
+if ( empty( $thumbnail_url ) ) {
+	$wrapper_classes .= ' godam-video-thumbnail--not-found';
+}
+
+$wrapper_attributes = get_block_wrapper_attributes(
+	array(
+		'class' => $wrapper_classes,
+	)
+);
 ?>
-<h2 <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
-	<?php _e( 'GoDAM Video Thumbnail', 'godam' ); ?>
-</h2>
+
+<div <?php echo wp_kses_data( $wrapper_attributes ); ?>>
+	<?php if ( ! empty( $thumbnail_url ) ) : ?>
+		<img src="<?php echo esc_url( $thumbnail_url ); ?>" alt="<?php echo esc_attr( $alt_text ); ?>" class="godam-video-thumbnail" />
+	<?php else : ?>
+		<div class="godam-video-thumbnail__fallback">
+		</div>
+</div>
+<?php endif; ?>
