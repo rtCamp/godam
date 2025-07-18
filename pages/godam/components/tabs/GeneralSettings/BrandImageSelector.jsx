8d4d3c20bdac1b2af1f9afe6c82b6dfff44238b6
Prachi Garg <72646230+prachigarg19@users.noreply.github.com>
2025-07-18 16:02:26 +0530

    feat: remove preview image for classic and bubble

--- pages/godam/components/tabs/GeneralSettings/BrandImageSelector.jsx
+++ pages/godam/components/tabs/GeneralSettings/BrandImageSelector.jsx
@@ -105,7 +105,7 @@ const BrandImageSelector = ( { mediaSettings, handleSettingChange } ) => {
 					{ __( 'Remove', 'godam' ) }
 				</Button>
 			) }
-			{ mediaSettings?.video_player?.brand_image && (
+			{ mediaSettings?.video_player?.brand_image && 'Bubble' !== mediaSettings?.video_player?.player_skin && 'Classic' !== mediaSettings?.video_player?.player_skin && (
 				<div className="mt-2 border-2 border-blue-700 rounded-lg p-2 block bg-gray-200 w-fit">
 					<img
 						src={ mediaSettings?.video_player?.brand_image }
