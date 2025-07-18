98a4af97d622dea7755ebd82c3aa96c2304fc584
Subodh Rajpopat <58080782+subodhr258@users.noreply.github.com>
2025-07-18 19:36:54 +0530

    chore: Remove Woo from release (#688)

--- inc/templates/godam-player.php
+++ inc/templates/godam-player.php
@@ -507,18 +507,6 @@ if ( $is_shortcode || $is_elementor_widget ) {
 							>
 							</div>
 							<?php
-							// WooCommerce layer.
-						elseif ( isset( $layer['type'] ) && 'woo' === $layer['type'] ) :
-							?>
-							<div
-								id="layer-<?php echo esc_attr( $instance_id . '-' . $layer['id'] ); ?>"
-								class="easydam-layer hidden hotspot-layer"
-								<?php if ( ! empty( $layer['bg_color'] ) ) : ?>
-									style="background-color: <?php echo esc_attr( $layer['bg_color'] ); ?>"
-								<?php endif; ?>
-							>
-							</div>
-							<?php
 						endif;
 					endforeach;
 					?>
