8ef4bde172bd0b96fad43eba529dcb8203d23288
lzzy12 <jhashivam2020@gmail.com>
2025-07-28 16:57:33 +0530

    chore: update godam svg logo

--- assets/src/blocks/common/godam-logo.js
+++ assets/src/blocks/common/godam-logo.js
@@ -1,14 +1,21 @@
 export const GodamLogo = () => (
-	<svg width="42" height="48" viewBox="0 0 42 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M12.5375 2.60374L2.87218 12.278L0.629929 10.0337C-0.661858 8.74069 0.140114 6.3454 2.06237 5.76691L12.5401 2.60637L12.5375 2.60374Z" fill="url(#paint0_linear_4091_45581)" />
+		<path d="M23.8957 3.00616L22.7461 6.98693L10.1983 19.5464L4.49218 13.835L17.0725 1.24306L20.9984 0.106175C22.8543 -0.430396 24.4317 1.14848 23.893 3.00347L23.8957 3.00616Z" fill="url(#paint1_linear_4091_45581)" />
+		<path d="M21.3775 11.4519L18.2259 21.9348C17.645 23.8606 15.2466 24.6638 13.9508 23.3669L11.7122 21.1262L21.3775 11.4519Z" fill="url(#paint2_linear_4091_45581)" />
 		<defs>
-			<linearGradient id="bg-gradient" x1="44.6%" y1="0.3%" x2="55.4%" y2="99.7%">
-				<stop offset="-9.3%" style={ { stopColor: '#AB3A6C', stopOpacity: 1 } } />
-				<stop offset="120.31%" style={ { stopColor: '#E6533A', stopOpacity: 1 } } />
+			<linearGradient id="paint0_linear_4091_45581" x1="13.2482" y1="23.4965" x2="23.3389" y2="13.4151" gradientUnits="userSpaceOnUse">
+				<stop stopColor="#AB3A6C" />
+				<stop offset="1" stopColor="#E6533A" />
+			</linearGradient>
+			<linearGradient id="paint1_linear_4091_45581" x1="13.2482" y1="23.4965" x2="23.3389" y2="13.4151" gradientUnits="userSpaceOnUse">
+				<stop stopColor="#AB3A6C" />
+				<stop offset="1" stopColor="#E6533A" />
+			</linearGradient>
+			<linearGradient id="paint2_linear_4091_45581" x1="13.2482" y1="23.4965" x2="23.3389" y2="13.4151" gradientUnits="userSpaceOnUse">
+				<stop stopColor="#AB3A6C" />
+				<stop offset="1" stopColor="#E6533A" />
 			</linearGradient>
 		</defs>
-
-		<path d="M24.7514 11.2995H0V4.80557C0 1.06434 4.09102 -1.23971 7.29236 0.706948L24.7514 11.3071V11.2995Z" fill="url(#bg-gradient)" />
-		<path d="M38.9103 27.9981L32.3099 32.0055H0V15.8315H32.3936L38.9103 19.7857C41.99 21.6563 41.99 26.1275 38.9103 27.9905V27.9981Z" fill="url(#bg-gradient)" />
-		<path d="M24.7058 36.7275L7.29236 47.2973C4.09102 49.2363 0 46.9323 0 43.191V36.7275H24.7058Z" fill="url(#bg-gradient)" />
 	</svg>
 );
