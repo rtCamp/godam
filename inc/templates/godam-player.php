e1eb34748b817793cc3f1a6edfe0cdb7ae52acd1
Juzar Bharmal <53657281+Juzar10@users.noreply.github.com>
2025-08-07 15:51:06 +0530

    Fix disable inline volume panel for Minimal and Pills player skins (#813)
    * fix: volume button disable is not respected for the GoDAM player settings
    
    * feat: add default volume panel settings to video control bar

--- inc/templates/godam-player.php
+++ inc/templates/godam-player.php
@@ -221,10 +221,14 @@ $video_setup = array(
 if ( ! empty( $control_bar_settings ) ) {
 	$video_setup['controlBar'] = $control_bar_settings; // contains settings specific to control bar.
 
-	// Define your default volumePanel setting.
-	$volume_panel_setting = array(
-		'inline' => ! in_array( $player_skin, array( 'Minimal', 'Pills' ), true ),
-	);
+	if ( isset( $control_bar_settings['volumePanel'] ) && empty( $control_bar_settings['volumePanel'] ) ) {
+		$volume_panel_setting = $control_bar_settings['volumePanel'];
+	} else {
+		// Define your default volumePanel setting.
+		$volume_panel_setting = array(
+			'inline' => ! in_array( $player_skin, array( 'Minimal', 'Pills' ), true ),
+		);
+	}
 
 	$video_setup['controlBar']['volumePanel'] = $volume_panel_setting;
 }
