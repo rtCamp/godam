3b6cc495f13b073f9e6f46feec52b0e2cb737113
Lakshyajeet Singh Goyal <lakshyajeet.goyal@rtcamp.com>
2025-08-14 17:44:22 +0530

    fix: add width as constants

--- assets/src/js/media-library/views/attachments.js
+++ assets/src/js/media-library/views/attachments.js
@@ -9,11 +9,15 @@ const Attachments = wp?.media?.view?.Attachments;
 export default Attachments?.extend( {
 
 	initialize() {
+		const BREAKPOINT_WIDTH = 640;
+		const SMALL_COLUMN_WIDTH = 135;
+		const LARGE_COLUMN_WIDTH = 200;
+
 		// Call the parent initialize method
 		Attachments.prototype.initialize.apply( this, arguments );
 
 		// Increase the column width for media attachments.
-		this.options.idealColumnWidth = window.innerWidth < 640 ? 135 : 200;
+		this.options.idealColumnWidth = window.innerWidth < BREAKPOINT_WIDTH ? SMALL_COLUMN_WIDTH : LARGE_COLUMN_WIDTH;
 
 		if ( Attachment ) {
 			/**
