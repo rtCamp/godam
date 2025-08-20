c0ff50794d9b32a820c26c6490f3f83f6f35ed47
Lakshyajeet Singh Goyal <lakshyajeet.goyal@rtcamp.com>
2025-08-20 17:54:17 +0530

    fix: prevent duplicate attachments for virtual media

--- inc/classes/rest-api/class-media-library.php
+++ inc/classes/rest-api/class-media-library.php
@@ -1222,6 +1222,7 @@ class Media_Library extends Base {
 		update_post_meta( $attach_id, 'rtgodam_transcoding_status', 'transcoded' );
 		update_post_meta( $attach_id, 'icon', $data['icon'] );
 		update_post_meta( $attach_id, 'rtgodam_hls_transcoded_url', esc_url_raw( $data['hls_url'] ?? '' ) );
+		update_post_meta( $attach_id, 'rtgodam_transcoding_job_id', $godam_id );
 
 		// Return the newly created media object.
 		return new \WP_REST_Response(
