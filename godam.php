4ec5b6c9c729e78652b3ad898287864bb3fbebb6
Devasheesh Kaul <devasheesh0909@gmail.com>
2025-08-26 12:06:12 +0530

    fix: possible multisite inconsitency and localization warning

--- godam.php
+++ godam.php
@@ -147,10 +147,27 @@ register_deactivation_hook( __FILE__, 'rtgodam_plugin_deactivate' );
  * Runs when the plugin is deleted.
  */
 function rtgodam_plugin_delete() {
-	// Delete options related to What's New page.
+	// Delete options and transients related to Whats New page.
 	// This is to ensure redirection on a fresh install.
-	delete_option( 'rtgodam_plugin_version' );
-	delete_option( '_transient_rtgodam_release_data' );
+	if ( is_multisite() ) {
+		// Get all blogs in the network and delete options from each blog.
+		$blogs = get_sites( array( 'fields' => 'ids' ) );
+
+		foreach ( $blogs as $blog_id ) {
+			switch_to_blog( $blog_id );
+
+			delete_option( 'rtgodam_plugin_version' );
+			delete_transient( 'rtgodam_show_whats_new' );
+			delete_transient( 'rtgodam_release_data' );
+
+			restore_current_blog();
+		}
+	} else {
+		// For single site, delete options directly.
+		delete_option( 'rtgodam_plugin_version' );
+		delete_transient( 'rtgodam_show_whats_new' );
+		delete_transient( 'rtgodam_release_data' );
+	}
 }
 
 register_uninstall_hook( __FILE__, 'rtgodam_plugin_delete' );
