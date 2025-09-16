f7d29293df641c9bde0ba333035139f5b52d13ee
KMchaudhary <kuldipkumar.chaudhary@rtcamp.com>
2025-09-16 10:59:26 +0530

    Configure videojs player to pretend user initial bandwidth of 14Mbps when video start play
    Configure initial bandwidth for GoDAM Video block edit page, GoDAM video player on frontend, Analytics page video player, and media library virtual media video player

--- pages/godam/components/tabs/VideoPlayer/VideoPlayer.jsx
+++ pages/godam/components/tabs/VideoPlayer/VideoPlayer.jsx
@@ -168,6 +168,15 @@ const VideoPlayer = () => {
 					backward: 10,
 				},
 			},
+			// VHS (HLS/DASH) initial configuration to prefer a ~14 Mbps start.
+			// This only affects the initial bandwidth guess; VHS will continue to measure actual throughput and adapt.
+			html5: {
+				vhs: {
+					bandwidth: 14_000_000, // Pretend network can do ~14 Mbps at startup
+					bandwidthVariance: 1.0, // allow renditions close to estimate
+					limitRenditionByPlayerDimensions: false, // don't cap by video element size
+				},
+			},
 		}, () => {
 			// Register the unified settings button component
 			const controlBar = player.getChild( 'controlBar' );
