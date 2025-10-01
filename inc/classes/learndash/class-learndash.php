b25e63c1fc637ab2479f5228c10fd4609f07c212
Vipul Patil <vipulrpatil.8@gmail.com>
2025-10-01 19:32:34 +0530

    FEAT: Add custom data attributes

--- inc/classes/learndash/class-learndash.php
+++ inc/classes/learndash/class-learndash.php
@@ -32,8 +32,11 @@ class LearnDash {
 	 * Setup WordPress hooks and filters.
 	 */
 	private function setup_hooks() {
-		add_action( 'admin_enqueue_scripts', array( $this, 'load_learndash_admin_integration_scripts' ) );
 		add_filter( 'learndash_settings_fields', array( $this, 'add_godam_settings' ), 10, 2 );
+		add_filter( 'ld_video_provider', array( $this, 'add_godam_provider' ), 10, 2 );
+		add_filter( 'godam_player_video_element_attributes', array( $this, 'modify_godam_player_video_element_attributes' ), 10, 1 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'load_learndash_admin_integration_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'load_learndash_frontend_integration_script' ) );
 	}
 
 	/**
@@ -58,7 +61,7 @@ class LearnDash {
 	 *
 	 * @return bool
 	 */
-	public function is_learndash_content() {
+	public function is_learndash_content(): bool {
 		global $post;
 
 		if ( ! isset( $post ) || ! isset( $post->post_type ) ) {
@@ -77,6 +80,18 @@ class LearnDash {
 
 		if ( $this->is_learndash_active() && $this->is_learndash_content() ) {
 			wp_enqueue_script( 'rtgodam-learndash-admin-integration', RTGODAM_URL . 'assets/build/js/godam-learndash-admin.min.js', array( 'jquery' ), filemtime( RTGODAM_PATH . 'assets/build/js/godam-learndash-admin.min.js' ), true );
+
+		}
+	}
+
+	/**
+	 * Load LearnDash frontend integration script.
+	 *
+	 * @since 1.4.4
+	 */
+	public function load_learndash_frontend_integration_script() {
+		if ( $this->is_learndash_active() && $this->is_learndash_content() ) {
+			wp_enqueue_script( 'rtgodam-learndash-block-integration', RTGODAM_URL . 'assets/build/js/godam-learndash-block.min.js', array( 'jquery' ), filemtime( RTGODAM_PATH . 'assets/build/js/godam-learndash-block.min.js' ), true );
 		}
 	}
 
@@ -110,7 +125,7 @@ class LearnDash {
 			'default'        => 'off',
 			'options'        => array(
 				'on' => esc_html__( 'GoDAM Video block added in this post will be used for video progression', 'godam' ),
-				''   => esc_html__( 'Off', 'godam' ),
+				''   => '',
 			),
 		);
 
@@ -137,5 +152,122 @@ class LearnDash {
 			return 'rtgodam';
 		}
 		return $video_provider;
+	}
+
+	/**
+	 * Add custom attributes to the godam/video block for LearnDash video progression.
+	 * This function checks if the video has been completed and modifies attributes accordingly.
+	 *
+	 * @param array $attributes Current attributes of the godam/video block.
+	 *
+	 * @return array
+	 */
+	public function modify_godam_player_video_element_attributes( array $attributes ): array {
+		global $post;
+
+		$content_video_completed = $this->is_content_video_completed( $post->ID );
+
+		$data_unique_id = ! empty( $attributes['data-unique-id'] ) ? '(rtgodam-' . $attributes['data-unique-id'] . ')' : '(rtgodam-' . wp_generate_uuid4() . ')';
+		$cookie_key     = $this->generate_cookie_key( $data_unique_id );
+
+		if ( $content_video_completed ) {
+			$attributes['data-video-cookie-key']  = $cookie_key;
+			$attributes['data-video-progression'] = 'false';
+			$attributes['data-video-provider']    = 'rtgodam';
+		} elseif ( $this->is_learndash_active() && $this->is_learndash_content() ) {
+			$attributes['data-video-cookie-key']  = $cookie_key;
+			$attributes['data-video-progression'] = 'true';
+			$attributes['data-video-provider']    = 'rtgodam';
+		}
+
+		return $attributes;
+	}
+
+	/**
+	 * Generate a unique cookie key for storing video progression.
+	 *
+	 * @param string $id  Unique ID of the godam/video block.
+	 *
+	 * @return string
+	 */
+	public function generate_cookie_key( string $id ): string {
+		$cookie_key = $this->generate_unique_key();
+
+		if ( ! empty( $id ) ) {
+			$cookie_key .= '_' . html_entity_decode( trim( $id ) );
+		}
+
+		return 'learndash-video-progress-' . md5( $cookie_key );
+	}
+
+	/**
+	 * Generate a unique key based on user ID, course ID, and post ID.
+	 *
+	 * @return string
+	 */
+	public function generate_unique_key(): string {
+		$post_id   = get_the_ID();
+		$course_id = learndash_get_course_id( $post_id );
+		$user_id   = (int) get_current_user_id();
+
+		return sprintf( '%d_%d_%d', $user_id, $course_id, $post_id );
+	}
+
+	/**
+	 * Check if the LearnDash content video is marked as completed.
+	 *
+	 * @return bool
+	 */
+	public function is_content_video_completed(): bool {
+		if ( ! function_exists( 'learndash_get_course_progress' ) ) {
+			return true;
+		}
+
+			global $post;
+			$progress = learndash_get_course_progress( null, $post->ID );
+			return ( ! empty( $progress['this'] ) ) && ( $progress['this'] instanceof \WP_Post ) && ( true === (bool) $progress['this']->completed );
+	}
+
+	/**
+	 * Get unique ID of the godam/video block from a post.
+	 *
+	 * @param int $post_id The ID of the post.
+	 *
+	 * @return array|null Attributes of the godam/video block, or null if not found.
+	 */
+	public function godam_get_video_block_id_attributes( int $post_id ): ?array {
+		$post = get_post( $post_id );
+
+		if ( ! $post || empty( $post->post_content ) ) {
+			return null;
+		}
+
+		$blocks = parse_blocks( $post->post_content );
+
+		if ( empty( $blocks ) ) {
+			return null;
+		}
+
+		// Recursive search in case block is inside a group/columns etc.
+		$find_block = function ( $blocks ) use ( &$find_block ) {
+			foreach ( $blocks as $block ) {
+				if ( 'godam/video' === $block['blockName'] ) {
+					return $block['attrs'] ?? array();
+				}
+
+				// Check inner blocks.
+				if ( ! empty( $block['innerBlocks'] ) ) {
+					$found = $find_block( $block['innerBlocks'] );
+					if ( null !== $found ) {
+						return $found;
+					}
+				}
+			}
+			return null;
+		};
+
+		$attrs = $find_block( $blocks );
+
+		return $attrs['uniqueId'];
 	}
 }
