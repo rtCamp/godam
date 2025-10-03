d5169284b7eb46d67cca3af453460210b05a81e8
Anuj-Rathore24 <anujsingh2409@gmail.com>
2025-10-03 19:09:01 +0530

    feat: Enqueue scripts for gfforms, ninjaform, cf7 forms and ninjaforms.

--- inc/templates/godam-player.php
+++ inc/templates/godam-player.php
@@ -479,6 +479,15 @@ function handle_assets( ...$handle_names ) {
 							<div id="layer-<?php echo esc_attr( $instance_id . '-' . $layer['id'] ); ?>" class="easydam-layer hidden" style="background-color: <?php echo isset( $layer['bg_color'] ) ? esc_attr( $layer['bg_color'] ) : '#FFFFFFB3'; ?>">
 								<div class="form-container">
 									<?php
+
+									if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+										if ( class_exists( 'GFForms' ) ) {
+											require_once GFCommon::get_base_path() . '/form_display.php';
+											GFFormDisplay::enqueue_form_scripts( null, true );
+											handle_assets( 'gform', 'gravity' );
+										}
+									}
+
 										$theme = ! empty( $layer['theme'] ) ? esc_attr( $layer['theme'] ) : '';
 										echo do_shortcode(
 											sprintf(
@@ -497,6 +506,15 @@ function handle_assets( ...$handle_names ) {
 								<div id="layer-<?php echo esc_attr( $instance_id . '-' . $layer['id'] ); ?>" class="easydam-layer hidden" style="background-color: <?php echo isset( $layer['bg_color'] ) ? esc_attr( $layer['bg_color'] ) : '#FFFFFFB3'; ?>">
 									<div class="form-container <?php echo esc_attr( 'godam' === $form_theme ? 'rtgodam-wpcf7-form' : '' ); ?>">
 										<?php
+
+										if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+											if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
+												wpcf7_enqueue_scripts();
+												wpcf7_enqueue_styles();
+												handle_assets( 'contact-form-7', 'wpcf7' );
+											}
+										}
+
 											echo do_shortcode(
 												sprintf(
 													"[contact-form-7 id='%d' title='false' ajax='true']",
@@ -540,6 +558,15 @@ function handle_assets( ...$handle_names ) {
 								<div id="layer-<?php echo esc_attr( $instance_id . '-' . $layer['id'] ); ?>" class="easydam-layer hidden" style="background-color: <?php echo isset( $layer['bg_color'] ) ? esc_attr( $layer['bg_color'] ) : '#FFFFFFB3'; ?>">
 									<div class="form-container">
 										<?php
+
+										if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+											if ( class_exists( 'SRFM\Inc\Frontend_Assets' ) ) {
+												SRFM\Inc\Frontend_Assets::enqueue_scripts_and_styles();
+
+											}
+											handle_assets( 'srfm', 'sureform' );
+										}
+
 											echo do_shortcode(
 												sprintf(
 													"[sureforms id='%d']",
@@ -640,12 +667,22 @@ function handle_assets( ...$handle_names ) {
 								<div id="layer-<?php echo esc_attr( $instance_id . '-' . $layer['id'] ); ?>" class="easydam-layer hidden <?php echo esc_attr( $form_type ); ?>" style="background-color: <?php echo isset( $layer['bg_color'] ) ? esc_attr( $layer['bg_color'] ) : '#FFFFFFB3'; ?>">
 									<div class="form-container">
 										<?php
+
+										if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+
+											if ( class_exists( 'Ninja_Forms' ) ) {
+												if ( class_exists( 'NF_Display_Render' ) ) {
+													Ninja_Forms()->display( $layer['ninja_form_id'] );
+												}
+												handle_assets( 'ninja', 'nf' );
+											}
 											echo do_shortcode(
 												sprintf(
 													"[ninja_form id='%d']",
 													intval( $layer['ninja_form_id'] )
 												)
 											);
+										}
 										?>
 									</div>
 								</div>
