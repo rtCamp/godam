1c5bb36270b4dc47edff72d4b91fe26b532da90f
KMchaudhary <kuldipkumar.chaudhary@rtcamp.com>
2025-10-07 10:06:21 +0530

    Refactor the GoDAM pages permission for core users roles

--- inc/classes/class-pages.php
+++ inc/classes/class-pages.php
@@ -165,7 +165,7 @@ class Pages {
 		add_menu_page(
 			__( 'GoDAM', 'godam' ),
 			__( 'GoDAM', 'godam' ),
-			'manage_options',
+			'edit_pages',
 			$this->menu_slug,
 			array( $this, 'render_dashboard_page' ),
 			'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTI1LjU1NzggMjAuMDkxMUw4LjA1NTg3IDM3LjU5M0wzLjQ2Mzk3IDMzLjAwMTFDMC44MTg1MjEgMzAuMzU1NiAyLjA4MjEgMjUuODMzNiA1LjcyMjI4IDI0Ljk0NjRMMjUuNTYzMiAyMC4wOTY0TDI1LjU1NzggMjAuMDkxMVoiIGZpbGw9IndoaXRlIi8+CjxwYXRoIGQ9Ik00Ny4zNzczIDIxLjg4NjdMNDUuNTQzOCAyOS4zODc1TDIyLjY5NzIgNTIuMjM0MUwxMS4yNjA1IDQwLjc5NzRMMzQuMTY2MiAxNy44OTE2TDQxLjU3MDMgMTYuMDc5NkM0NS4wNzA2IDE1LjIyNDcgNDguMjMyMyAxOC4zODYzIDQ3LjM3MiAyMS44ODEzTDQ3LjM3NzMgMjEuODg2N1oiIGZpbGw9IndoaXRlIi8+CjxwYXRoIGQ9Ik00My41MDU5IDM4LjEwMzZMMzguNjY2NyA1Ny44OTA3QzM3Ljc3NDEgNjEuNTI1NSAzMy4yNTIxIDYyLjc4OTEgMzAuNjA2NiA2MC4xNDM2TDI2LjAzNjMgNTUuNTczMkw0My41MDU5IDM4LjEwMzZaIiBmaWxsPSJ3aGl0ZSIvPgo8L3N2Zz4K',
@@ -180,7 +180,7 @@ class Pages {
 			$this->menu_slug,
 			__( 'Dashboard', 'godam' ),
 			__( 'Dashboard', 'godam' ),
-			'manage_options',
+			'edit_pages', // Dashboard is accessible to editors and above.
 			$this->menu_slug,
 			array( $this, 'render_dashboard_page' ),
 			1
@@ -190,7 +190,7 @@ class Pages {
 			$this->menu_slug,
 			__( 'Video Editor', 'godam' ),
 			__( 'Video Editor', 'godam' ),
-			'publish_posts',
+			'upload_files', // Video Editor is accessible to authors and above.
 			$this->video_editor_slug,
 			array( $this, 'render_video_editor_page' ),
 			3
@@ -200,7 +200,7 @@ class Pages {
 			$this->menu_slug,
 			__( 'Analytics', 'godam' ),
 			__( 'Analytics', 'godam' ),
-			'edit_posts',
+			'upload_files', // Analytics is accessible to authors and above.
 			$this->analytics_slug,
 			array( $this, 'render_analytics_page' ),
 			2
@@ -212,7 +212,7 @@ class Pages {
 				$this->menu_slug,
 				__( 'Tools', 'godam' ),
 				__( 'Tools', 'godam' ),
-				'manage_options',
+				'manage_options', // Tools is accessible to admins and above.
 				$this->tools_slug,
 				array( $this, 'render_tools_page' ),
 				5
@@ -223,7 +223,7 @@ class Pages {
 			$this->menu_slug,
 			__( 'Settings', 'godam' ),
 			__( 'Settings', 'godam' ),
-			'edit_pages',
+			'manage_options', // Settings is accessible to admins and above.
 			$this->settings_slug,
 			array( $this, 'render_godam_page' ),
 			6
