c610baf0e55c2b6bc9d89f08865d58f00b651d56
KMchaudhary <kuldipkumar.chaudhary@rtcamp.com>
2025-10-10 10:44:27 +0530

    Fix null check for VM analytics, and convert VM job_id into original attachment ID

--- assets/src/js/godam-player/analytics.js
+++ assets/src/js/godam-player/analytics.js
@@ -141,7 +141,7 @@ if ( ! window.pageLoadEventTracked ) {
 		// Collect all video IDs
 		const videoIds = Array.from( videos )
 			.map( ( video ) => video.getAttribute( 'data-id' ) )
-			.filter( ( id ) => id !== null && id !== '' ) // Null and empty string check
+			.filter( ( id ) => id !== null && id !== '' && ! isNaN( id ) ) // Null and empty string check
 			.map( ( id ) => parseInt( id, 10 ) ); // Convert to integer
 
 		// Send a single page_load request with all video IDs
