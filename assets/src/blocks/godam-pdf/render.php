2734aa9848c9174b7c8e75b5557387275f5ee73c
KMchaudhary <kuldipkumar.chaudhary@rtcamp.com>
2025-12-02 10:04:38 +0530

    Take a pull from remote branch and resolve merge conflicts

--- assets/src/blocks/godam-pdf/render.php
+++ assets/src/blocks/godam-pdf/render.php
@@ -9,44 +9,44 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$attachment_id = ! empty( $attributes['id'] ) ? ( is_numeric( $attributes['id'] ) ? intval( $attributes['id'] ) : sanitize_text_field( $attributes['id'] ) ) : null;
-$caption       = ! empty( $attributes['caption'] ) ? $attributes['caption'] : '';
-$height        = ! empty( $attributes['height'] ) ? intval( $attributes['height'] ) : 600;
-$src           = ! empty( $attributes['src'] ) ? esc_url( $attributes['src'] ) : '';
+$godam_attachment_id = ! empty( $attributes['id'] ) ? ( is_numeric( $attributes['id'] ) ? intval( $attributes['id'] ) : sanitize_text_field( $attributes['id'] ) ) : null;
+$godam_caption       = ! empty( $attributes['caption'] ) ? $attributes['caption'] : '';
+$godam_height        = ! empty( $attributes['height'] ) ? intval( $attributes['height'] ) : 600;
+$godam_src           = ! empty( $attributes['src'] ) ? esc_url( $attributes['src'] ) : '';
 
-if ( ! $attachment_id && empty( $src ) ) {
+if ( ! $godam_attachment_id && empty( $godam_src ) ) {
 	return;
 }
 
-$sources = array();
-if ( ! empty( $attachment_id ) && is_numeric( $attachment_id ) ) { 
-	$pdf_url            = wp_get_attachment_url( $attachment_id );
-	$pdf_transcoded_url = get_post_meta( $attachment_id, 'rtgodam_transcoded_url', true );
+$godam_sources = array();
+if ( ! empty( $godam_attachment_id ) && is_numeric( $godam_attachment_id ) ) { 
+	$pdf_url            = wp_get_attachment_url( $godam_attachment_id );
+	$pdf_transcoded_url = get_post_meta( $godam_attachment_id, 'rtgodam_transcoded_url', true );
 	if ( ! empty( $pdf_transcoded_url ) ) {
-		$sources[] = $pdf_transcoded_url;
+		$godam_sources[] = $pdf_transcoded_url;
 	}
 	if ( ! empty( $pdf_url ) ) {
-		$sources[] = $pdf_url;
+		$godam_sources[] = $pdf_url;
 	}
 } else {
-	$sources[] = $src;
+	$godam_sources[] = $godam_src;
 }
 
-if ( empty( $sources ) ) {
+if ( empty( $godam_sources ) ) {
 	return;
 }
 
 ?>
 
 <figure <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
-	<div class="godam-pdf-wrapper" style="height: <?php echo esc_attr( $height ); ?>px;">
+	<div class="godam-pdf-wrapper" style="height: <?php echo esc_attr( $godam_height ); ?>px;">
 		<object
 			id="pdfObject"
 			type="application/pdf"
 			width="100%"
 			height="100%"
-			data="<?php echo esc_url( $sources[0] ); ?>"
-			data-sources="<?php echo esc_attr( wp_json_encode( $sources ) ); ?>"
+			data="<?php echo esc_url( $godam_sources[0] ); ?>"
+			data-sources="<?php echo esc_attr( wp_json_encode( $godam_sources ) ); ?>"
 		>
 			<p>
 				<?php
@@ -54,7 +54,7 @@ if ( empty( $sources ) ) {
 					sprintf(
 						/* translators: %s: PDF download URL */
 						__( 'Your browser does not support PDFs. <a href="%s">Download the PDF</a>.', 'godam' ),
-						esc_url( $sources[0] )
+						esc_url( $godam_sources[0] )
 					)
 				);
 				?>
@@ -62,9 +62,9 @@ if ( empty( $sources ) ) {
 		</object>
 	</div>
 
-	<?php if ( $caption ) : ?>
+	<?php if ( $godam_caption ) : ?>
 		<figcaption class="wp-element-caption">
-			<?php echo wp_kses_post( $caption ); ?>
+			<?php echo wp_kses_post( $godam_caption ); ?>
 		</figcaption>
 	<?php endif; ?>
 </figure>
