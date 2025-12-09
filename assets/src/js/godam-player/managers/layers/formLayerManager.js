2e904cf0ecaa5f740904916eacdd29a69e56843c
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2025-12-09 17:42:22 +0530

    fix: Add Inner HTML for SVG

--- assets/src/js/godam-player/managers/layers/formLayerManager.js
+++ assets/src/js/godam-player/managers/layers/formLayerManager.js
@@ -104,17 +104,8 @@ export default class FormLayerManager {
 		skipButton.textContent = skipText;
 		skipButton.classList.add( 'skip-button' );
 
-		const arrowIcon = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
-		arrowIcon.setAttribute( 'viewBox', '0 0 320 512' );
-		arrowIcon.setAttribute( 'width', '12' );
-		arrowIcon.setAttribute( 'height', '12' );
-		arrowIcon.setAttribute( 'fill', 'currentColor' );
-		arrowIcon.setAttribute( 'aria-hidden', 'true' );
-
-		const path = document.createElementNS( 'http://www.w3.org/2000/svg', 'path' );
-		path.setAttribute( 'd', 'M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z' );
-		arrowIcon.appendChild( path );
-
+		const arrowIcon = document.createElement( 'span' );
+		arrowIcon.innerHTML = `<svg viewBox="0 0 320 512" width="12" height="12" fill="currentColor" aria-hidden="true"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>`;
 		skipButton.appendChild( arrowIcon );
 
 		return skipButton;
