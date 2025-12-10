d9fb421dfc09e484f6b1079db17fc7302428191e
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2025-12-10 15:53:27 +0530

    docs: Update version and changelog for 1.4.8

--- inc/classes/assets/class-ima-assets.php
+++ inc/classes/assets/class-ima-assets.php
@@ -19,7 +19,7 @@ class IMA_Assets {
 	 * Constructor register hooks if not already registered.
 	 *
 	 * @return void
-	 * @since n.e.x.t
+	 * 1.4.8t
 	 */
 	protected function __construct() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
@@ -29,7 +29,7 @@ class IMA_Assets {
 	 * Enqueue the IMA SDK.
 	 *
 	 * @return void
-	 * @since n.e.x.t
+	 * 1.4.8t
 	 */
 	public function enqueue_scripts() {
 		// No need to set dns-prefetch resource hint for IMA SDK as WordPress automatically does that for 3rd party scripts.
