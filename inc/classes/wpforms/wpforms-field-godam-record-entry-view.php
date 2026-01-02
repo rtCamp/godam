c483ba49d8234bb0f0fa159b23482d566b10254a
AhmarZaidi <ahmarzaidi07@gmail.com>
2026-01-02 17:04:57 +0530

    Add audio recording support for WPForms GoDAM Recorder
    - Import Uppy audio CSS styles in WPForms stylesheet
    - Add audio player styles matching Ninja Forms implementation
    - Remove inline styles from entry view/edit templates
    - Detect audio files and render audio player conditionally
    - Set correct job_type for audio transcoding requests

--- inc/classes/wpforms/wpforms-field-godam-record-entry-view.php
+++ inc/classes/wpforms/wpforms-field-godam-record-entry-view.php
@@ -18,6 +18,15 @@ $godam_attachment_name    = basename( $value );
 $godam_transcoded_url     = WPForms_Integration_Helper::get_transcoded_url( $godam_form_id, $godam_entry_id, $godam_field_id );
 $godam_hls_transcoded_url = WPForms_Integration_Helper::get_hls_transcoded_url( $godam_form_id, $godam_entry_id, $godam_field_id );
 $godam_transcoded_status  = WPForms_Integration_Helper::get_transcoded_status( $godam_form_id, $godam_entry_id, $godam_field_id );
+
+// Detect if this is an audio file.
+$file_type = wp_check_filetype( $godam_attachment_url );
+$is_audio  = strpos( $file_type['type'], 'audio' ) !== false;
+
+// Handle .webm audio files.
+if ( 'webm' === $file_type['ext'] && godam_is_audio_file_by_name( $godam_attachment_url ) ) {
+	$is_audio = true;
+}
 ?>
 
 <div class="godam-video-preview">
@@ -42,11 +51,21 @@ $godam_transcoded_status  = WPForms_Integration_Helper::get_transcoded_status( $
 		<?php endif; ?>
 	</div>
 
-	<?php
-		$godam_thumbnail_url = ''; // Default empty thumbnail.
-		// No need to escape here, the entire template will be returned as strings,
-		// which will be later on escaped using wp_kses_post() by WPForms before rendering the field.
-		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		echo force_balance_tags( do_shortcode( "[godam_video poster='{$godam_thumbnail_url}' src='{$godam_attachment_url}' transcoded_url='{$godam_transcoded_url}']" ) );
-	?>
+	<?php if ( $is_audio ) : ?>
+		<audio controls>
+			<?php if ( $godam_transcoded_url ) : ?>
+				<source src="<?php echo esc_url( $godam_transcoded_url ); ?>" type="audio/mpeg">
+			<?php endif; ?>
+			<source src="<?php echo esc_url( $godam_attachment_url ); ?>" type="<?php echo esc_attr( $file_type['type'] ); ?>">
+			<?php esc_html_e( 'Your browser does not support the audio element.', 'godam' ); ?>
+		</audio>
+	<?php else : ?>
+		<?php
+			$godam_thumbnail_url = ''; // Default empty thumbnail.
+			// No need to escape here, the entire template will be returned as strings,
+			// which will be later on escaped using wp_kses_post() by WPForms before rendering the field.
+			// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo force_balance_tags( do_shortcode( "[godam_video poster='{$godam_thumbnail_url}' src='{$godam_attachment_url}' transcoded_url='{$godam_transcoded_url}']" ) );
+		?>
+	<?php endif; ?>
 </div>
