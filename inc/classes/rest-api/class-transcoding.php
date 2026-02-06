068140c7589d405cedd92d95c370642f7f870d95
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2026-02-06 19:17:45 +0530

    Fix - Manual Transcode (#1624)
    * fix: Always set retranscode to true in Tools page
    
    * fix: Variable naming
    
    * fix: Update variable name for clarity

--- inc/classes/rest-api/class-transcoding.php
+++ inc/classes/rest-api/class-transcoding.php
@@ -604,18 +604,17 @@ class Transcoding extends Base {
 		$wp_metadata              = array();
 		$wp_metadata['mime_type'] = $mime_type;
 
-		// Check if media has been transcoded before and has a job ID.
-		$transcoded_url   = get_post_meta( $attachment_id, 'rtgodam_transcoded_url', true );
-		$existing_job_id  = get_post_meta( $attachment_id, 'rtgodam_transcoding_job_id', true );
-		$is_retranscoding = ! empty( $transcoded_url ) || ! empty( $existing_job_id );
+		// This is a manual retranscode request from the tools page.
+		// Always bypass the godam_auto_transcode_on_upload filter to allow transcoding even if the filter returns false.
+		$manual_retranscode = true;
 
 		// Retranscode the media.
 		if ( preg_match( '/image/i', $mime_type ) ) {
 			$transcoder = Media_Library_Ajax::get_instance();
-			$transcoder->upload_media_to_frappe_backend( $attachment_id, $is_retranscoding );
+			$transcoder->upload_media_to_frappe_backend( $attachment_id, $manual_retranscode );
 		} else {
 			$transcoder = new \RTGODAM_Transcoder_Handler( true );
-			$transcoder->wp_media_transcoding( $wp_metadata, $attachment_id, true, $is_retranscoding );
+			$transcoder->wp_media_transcoding( $wp_metadata, $attachment_id, true, $manual_retranscode );
 		}
 
 		// Check if the transcoding job ID is set.
