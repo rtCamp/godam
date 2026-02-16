e0d9c899432c6696e835d98b29fcd8df43156673
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2026-02-16 17:53:19 +0530

    fix: Improve rest routes for Media Entry for subsite

--- assets/src/js/media-library/views/godam-media-frame-shared.js
+++ assets/src/js/media-library/views/godam-media-frame-shared.js
@@ -12,6 +12,7 @@
 import { getQuery } from '../utility.js';
 
 const l10n = wp?.media?.view?.l10n;
+const restURL = window.godamRestRoute?.url || window.wpApiSettings?.root || '/wp-json/';
 
 /**
  * Check if the current frame is a featured image context.
@@ -132,11 +133,8 @@ const GoDAMMediaFrameShared = {
 		const selected = selection?.first();
 		const data = selected.attributes;
 
-		// API call to website to create the attachment.
-		const mediaEntryUrl = window.godamRestRoute?.url
-			? `${ window.godamRestRoute.url }godam/v1/media-library/create-media-entry`
-			: '/wp-json/godam/v1/media-library/create-media-entry';
-		fetch( mediaEntryUrl, {
+		// API call to website to create the attachment in the current site context.
+		fetch( window.pathJoin( [ restURL, '/godam/v1/media-library/create-media-entry' ] ), {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
