b7bf6b2a2668707bffb6a35a2384d19ee9cd64a5
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2026-03-05 17:28:05 +0530

    fix: API Key valid cache issue

--- godam.php
+++ godam.php
@@ -161,6 +161,9 @@ function rtgodam_plugin_delete() {
 			delete_option( 'rtgodam_welcome_completed' );
 			delete_option( 'rtgodam_show_whats_new' );
 			delete_option( 'rtgodam_show_welcome' );
+			delete_option( 'rtgodam_user_data' );
+			delete_option( 'rtgodam-api-key-status' );
+			delete_option( 'rtgodam-api-key-error-since' );
 			delete_transient( 'rtgodam_release_data' );
 
 			restore_current_blog();
@@ -171,6 +174,9 @@ function rtgodam_plugin_delete() {
 		delete_option( 'rtgodam_welcome_completed' );
 		delete_option( 'rtgodam_show_whats_new' );
 		delete_option( 'rtgodam_show_welcome' );
+		delete_option( 'rtgodam_user_data' );
+		delete_option( 'rtgodam-api-key-status' );
+		delete_option( 'rtgodam-api-key-error-since' );
 		delete_transient( 'rtgodam_release_data' );
 	}
 }
