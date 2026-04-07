5a4728c0ba909d2c5e6240a59dcb42c8e145fd8d
KMchaudhary <kuldipkumar.chaudhary@rtcamp.com>
2026-04-07 19:29:55 +0530

    Remove video carousel functionality and associated styles from WooCommerce integration

--- webpack.config.js
+++ webpack.config.js
@@ -223,13 +223,11 @@ const woocommerceIntegration = {
 		'admin/wc-admin-featured-video-gallery': path.resolve( process.cwd(), 'integrations', 'woocommerce', 'assets', 'js', 'admin', 'wc-admin-featured-video-gallery.js' ),
 
 		// Frontend scripts
-		'wc-video-carousel': path.resolve( process.cwd(), 'integrations', 'woocommerce', 'assets', 'js', 'single-product-story', 'wc-video-carousel.js' ),
 		'wc-featured-video-gallery': path.resolve( process.cwd(), 'integrations', 'woocommerce', 'assets', 'js', 'featured-video', 'wc-featured-video-gallery.js' ),
 		'wc-woo-layer-cart-url-editor': path.resolve( process.cwd(), 'integrations', 'woocommerce', 'assets', 'js', 'woocommerce-layer', 'wc-woo-layer-cart-url-editor.js' ),
 		'wc-woo-global-script': path.resolve( process.cwd(), 'integrations', 'woocommerce', 'assets', 'js', 'global-video-popup', 'index.js' ),
 
 		// CSS files
-		'godam-video-carousel': path.resolve( process.cwd(), 'integrations', 'woocommerce', 'assets', 'css', 'godam-video-carousel.scss' ),
 		'godam-featured-video': path.resolve( process.cwd(), 'integrations', 'woocommerce', 'assets', 'css', 'godam-featured-video.scss' ),
 		'godam-product-editor-gallery': path.resolve( process.cwd(), 'integrations', 'woocommerce', 'assets', 'css', 'godam-product-editor-gallery.scss' ),
 		'godam-product-gallery': path.resolve( process.cwd(), 'integrations', 'woocommerce', 'assets', 'css', 'godam-product-gallery.scss' ),
