9b4cb4fb1bcaff35e826ce5caea6f952eede2bed
KMchaudhary <kuldipkumar.chaudhary@rtcamp.com>
2026-04-23 12:57:58 +0530

    fix variable cashing warning

--- inc/classes/shortcodes/class-godam-player.php
+++ inc/classes/shortcodes/class-godam-player.php
@@ -207,9 +207,9 @@ class GoDAM_Player {
 		if ( isset( $attributes['show_share_button'] ) && '' !== $attributes['show_share_button'] && ( ! isset( $attributes['showShareButton'] ) || false === $attributes['showShareButton'] ) ) {
 			$attributes['showShareButton'] = $attributes['show_share_button'];
 		}
-		$raw_has_performance_mode = is_array( $atts ) && array_key_exists( 'performance_mode', $atts );
-		$raw_has_performanceMode  = is_array( $atts ) && array_key_exists( 'performanceMode', $atts );
-		if ( $raw_has_performance_mode && ! $raw_has_performanceMode && isset( $attributes['performance_mode'] ) && '' !== $attributes['performance_mode'] ) {
+		$raw_has_performance_mode            = is_array( $atts ) && array_key_exists( 'performance_mode', $atts );
+		$raw_has_performance_mode_camel_case = is_array( $atts ) && array_key_exists( 'performanceMode', $atts );
+		if ( $raw_has_performance_mode && ! $raw_has_performance_mode_camel_case && isset( $attributes['performance_mode'] ) && '' !== $attributes['performance_mode'] ) {
 			$attributes['performanceMode'] = $attributes['performance_mode'];
 		}
 
