9c0c275e85fb8440fcf5b8b4ab213a255da4facf
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2026-04-24 19:58:13 +0530

    fix: Grammatical errors

--- pages/video-editor/components/cta/ImageCTA.js
+++ pages/video-editor/components/cta/ImageCTA.js
@@ -143,7 +143,7 @@ const ImageCTA = ( { layerID } ) => {
 			 * This handles the case for the uploader tab of WordPress media library.
 			 */
 			if ( attachment.type !== 'image' ) {
-				showNotice( __( 'Only Image file is allowed', 'godam' ), 'error' );
+				showNotice( __( 'Only Image files are allowed', 'godam' ), 'error' );
 				return;
 			}
 
