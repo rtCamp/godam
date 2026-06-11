d6f7f99e6219034d435a045d1d78bb3d35b0d9b5
Subodh Rajpopat <subodh.rajpopat@rtcamp.com>
2026-06-11 13:11:43 +0530

    chore: remove Query Loop blocks (video thumbnail & duration) after CPT removal

--- assets/src/blocks/godam-video-thumbnail/save.js
+++
@@ -1,9 +0,0 @@
-/**
- * The save function for the GoDAM Video Thumbnail block.
- * This block is dynamic, so it does not save any content.
- *
- * @return {void}
- */
-export default function save() {
-	return null;
-}
